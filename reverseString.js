7b6b717acc89b5f459b211036b5a3fa51d627fd8
Alejandro <alejandrochang10@gmail.com>
2018-12-24 18:45:11 -0800

    reverse solution 2

--- reverseString.js
+++ reverseString.js
@@ -1,7 +1,18 @@
+// solution 1
 function reverse(str) {
   return str.split('').reverse().join('');
 }
 
-console.log(reverse('racecar')); // racecar
-console.log(reverse('hello')); // olleh
-console.log(reverse('buybye')); // eybyub
+// solution 2
+function reverse2(str) {
+  let result = '';
+  for (let i = str.length - 1; i >= 0; i--) {
+    result += str[i];
+  }
+
+  return result;
+}
+
+console.log(reverse2('racecar')); // racecar
+console.log(reverse2('hello')); // olleh
+console.log(reverse2('buybye')); // eybyub
