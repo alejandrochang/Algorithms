989bf739a3c7d797a3b0690c81e677670d37aa64
Alejandro <alejandrochang10@gmail.com>
2018-12-24 18:35:26 -0800

    reverse string problem

---
+++ reverseString.js
@@ -0,0 +1,7 @@
+function reverse(str) {
+  return str.split('').reverse().join('');
+}
+
+console.log(reverse('racecar')); // racecar
+console.log(reverse('hello')); // olleh
+console.log(reverse('buybye')); // eybyub
