eef0e7e7492f06dfbd03e129039494935c4fc98b
Alejandro <alejandrochang10@gmail.com>
2018-12-25 18:50:29 -0800

    max char complete

--- maxchar.js
+++ maxchar.js
@@ -6,9 +6,44 @@
 // maxChar("apple 1231111") === "1"
 
 function maxChar(str) {
-  
+  let hashMap = {};
+  let count = 0;
+  let mostCommonChar;
+
+  for (let char of str) {
+    hashMap[char] = hashMap[char] + 1 || 1;
+  }
+
+  for (let ch in hashMap) {
+    if (hashMap[ch] > count) {
+      count = hashMap[ch];
+      mostCommonChar = ch;
+    }
+  }
+
+  return mostCommonChar;
 }
 
 
 console.log(maxChar("abcccccccd")); // === "c"
 console.log(maxChar("apple 1231111")); // === "1"
+
+
+
+
+/*
+Both are equal:
+
+for (let char of str) {
+  if (!hashMap[char]) {
+    hashMap[char] = 1;
+  } else {
+    hashMap[char] += 1;
+  }
+}
+
+for (let char of str) {
+  hashMap[char] = hashMap[char] + 1 || 1;
+}
+
+*/
