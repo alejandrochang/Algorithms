e515ab67944c8deb565f41389c7698b4a95ee167
Alejandro <alejandrochang10@gmail.com>
2018-12-25 19:57:10 -0800

    maxchar complete

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -4,7 +4,7 @@
 // for multiples of 3, "buzz" for multiples of 5 and "Fizzbuzz" for both multiples
 
 function fizzbuzz(number) {
-  for (let i = 0; i < number; i++) {
+  for (let i = 1; i < number; i++) {
     if (i % 3 === 0 && i % 5 === 0) {
       console.log("Fizzbuzz")
     } else if (i % 3 === 0) {
