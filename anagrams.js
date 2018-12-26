2528eb2c953c0ce6e2c7aebea2d97ccde9e38e8a
Alejandro <alejandrochang10@gmail.com>
2018-12-25 22:02:34 -0800

    anagram solution 1

--- anagrams.js
+++ anagrams.js
@@ -10,29 +10,53 @@
 
 function anagrams(stringA, stringB) { 
   stringA = stringA.replace(/[^\w]/g, "").toLowerCase();
-  stringB = stringB.replace(/[^\w]/g, "").toLowerCase();
+  stringA = stringB.replace(/[^\w]/g, "").toLowerCase();
 
-  let charMapA = {};
-  let charMapB = {};
+  let hashMapA = buildCharMap(stringA);
+  let hashMapB = buildCharMap(stringB);
 
-  for (let ch of stringA) {
-    charMapA[ch] = charMapA[ch] + 1 || 1;
-  }
-
-  for (let ch of stringB) {
-    charMapB[ch] = charMapB[ch] + 1 || 1;
-  }
-
-  for (let char in charMapA) {
-    if (charMapA[char] !== charMapB[char]) {
+  for (let char in hashMapA) {
+    if (hashMapA[char] !== hashMapB[char]) {
       return false;
     }
   }
-
+  
   return true;
 }
 
+function buildCharMap(str) {
+  let charMap = {};
+  for (let ch of str) {
+    charMap[ch] = charMap[ch] += 1 || 1;
+  }
 
-  console.log(anagrams('rail safety', 'fairy tales')) // --> True
-  console.log(anagrams('RAIL! SAFETY!', 'fairy tales')) // --> True
-  console.log(anagrams('Hi there', 'Bye there')) // --> False
+  return charMap;
+}
+console.log(anagrams('rail safety', 'fairy tales')) // --> True
+console.log(anagrams('RAIL! SAFETY!', 'fairy tales')) // --> True
+console.log(anagrams('Hi there', 'Bye there')) // --> False
+
+
+// function anagrams(stringA, stringB) {
+//   stringA = stringA.replace(/[^\w]/g, "").toLowerCase();
+//   stringB = stringB.replace(/[^\w]/g, "").toLowerCase();
+
+//   let charMapA = {};
+//   let charMapB = {};
+
+//   for (let ch of stringA) {
+//     charMapA[ch] = charMapA[ch] + 1 || 1;
+//   }
+
+//   for (let ch of stringB) {
+//     charMapB[ch] = charMapB[ch] + 1 || 1;
+//   }
+
+//   for (let char in charMapA) {
+//     if (charMapA[char] !== charMapB[char]) {
+//       return false;
+//     }
+//   }
+
+//   return true;
+// }
