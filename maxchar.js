03b3fde46f38ba20aa9622774d6205e9c5aa15ca
Alejandro <alejandrochang10@gmail.com>
2018-12-25 17:06:14 -0800

    maxchar problem

---
+++ maxchar.js
@@ -0,0 +1,14 @@
+// --- Directions
+// Given a string, return the character that is most
+// commonly used in the string.
+// --- Examples
+// maxChar("abcccccccd") === "c"
+// maxChar("apple 1231111") === "1"
+
+function maxChar(str) {
+  
+}
+
+
+console.log(maxChar("abcccccccd")); // === "c"
+console.log(maxChar("apple 1231111")); // === "1"
