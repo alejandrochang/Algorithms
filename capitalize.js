996a958bad13f4b2eb292e26bd658431dc3360d4
Alejandro <alejandrochang10@gmail.com>
2018-12-26 16:17:36 -0800

    capitalize problem

--- capitalize.js
+++ capitalize.js
@@ -4,13 +4,17 @@
 // return the capitalized string.
 
 function capitalize(str) {
-  let words = str.split(' ');
-  
-  for (let i = 0; i < words.length; i++) {
-    words[i] = words[i][0].toUpperCase() + words[i].slice(1);
+  let result = str[0].toUpperCase();
+
+  for (let i = 1; i < str.length; i++) {
+    if (str[i - 1] === ' ') {
+      result += str[i].toUpperCase();
+    } else {
+      result += str[i];
+    }
   }
 
-  return words.join(' ');
+  return result;
 }
 
 
@@ -18,3 +22,24 @@ console.log(capitalize('a short sentence')) // --> 'A Short Sentence'
 console.log(capitalize('a lazy fox')) // --> 'A Lazy Fox'
 console.log(capitalize('look, it is working!')) // --> 'Look, It Is Working!'
 
+
+// function capitalize(str) {
+//   let words = str.split(' ');
+
+//   for (let i = 0; i < words.length; i++) {
+//     words[i] = words[i][0].toUpperCase() + words[i].slice(1);
+//   }
+
+//   return words.join(' ');
+// }
+
+
+// function capitalize(str) {
+//   let words = [];
+
+//   for (let word of str.split(" ")) {
+//     words.push(word[0].toUpperCase() + word.slice(1));
+//   }
+
+//   return words.join(" ");
+// }
