c9172f2820e7f2e3f47058f057cd17c6dceae1e0
Alejandro <alejandrochang10@gmail.com>
2018-12-31 15:50:17 -0800

    dfs and bfs on tree traversals

---
+++ trees.js
@@ -0,0 +1,47 @@
+// Trees 
+
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.children = [];
+  }
+
+  add(data) {
+    this.children.push(new Node(data));
+  }
+
+  remove(data) {
+    this.children = sthis.children.filter(node => {
+      return node.data !== data;
+    })
+  }
+}
+
+class Tree {
+  constructor(){
+    this.root = null;
+  }
+
+  bfs(fn) {
+    const arr = [this.root];
+    while (arr.length) {
+      const node = arr.shift();
+      arr.push(...node.children);
+      fn(node);
+    }
+  }
+
+  dfs(fn) {
+    const arr = [this.root];
+    while (arr.length) {
+      const node = arr.shift();
+      arr.unshift(...node.children);
+      fn(node);
+    }
+  }
+}
+
+const node = new Node(1);
+const tree = new Tree();
+tree.root = node;
+console.log(tree.root); // Node { data: 1, children: [] }
