1522ce1e2f2ea4471ed8a662eba0461aa8c235da
Alejandro <alejandrochang10@gmail.com>
2019-01-09 22:11:21 -0800

    incrementor problem

--- incrementor.js
+++ incrementor.js
@@ -3,10 +3,10 @@ b = [1, 3, 5, 9]; // 7
 
 
 function incrementor(arr) {
-  let step;
+  let step = null;
 
-  for (let i = 0; i < arr.length - 1; i++) {
-    if (arr[i+ 1] - arr[i] === arr[i + 2] - arr[i + 1]) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i + 1] - arr[i] === arr[i + 2] - arr[i + 1]) {
       step = arr[i + 1] - arr[i];
     }
   }
