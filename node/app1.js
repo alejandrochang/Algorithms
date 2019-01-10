e232ced12df26d984d8643fec34b0cc3748c8e02
Alejandro <alejandrochang10@gmail.com>
2019-01-10 12:06:37 -0800

    objexts and object literals

--- node/app1.js
+++ node/app1.js
@@ -1,1 +1,13 @@
-require('./greet')
+// let greet = require('./greet');
+
+var person = {
+  firstName: 'Alejandro',
+  lastName: 'Chang',
+  greet: function() {
+    console.log('Hello, ' + this.firstName + ' ' + this.lastName);
+  }
+}
+
+person.greet();
+
+console.log(person['firstName']) // Alejandro 
--- node/greet.js
+++ node/greet.js
@@ -1,1 +1,5 @@
-console.log('Hello');
+var greet = function() {
+  console.log('Hello')
+}
+
+module.exports = greet;
