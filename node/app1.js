df8843c0b0a8cef8d8b5ea6ba731c339c310a640
Alejandro <alejandrochang10@gmail.com>
2019-01-10 17:12:31 -0800

    minimum stack element

--- node/app1.js
+++ node/app1.js
@@ -1,13 +1,30 @@
 // let greet = require('./greet');
 
-var person = {
-  firstName: 'Alejandro',
-  lastName: 'Chang',
-  greet: function() {
-    console.log('Hello, ' + this.firstName + ' ' + this.lastName);
-  }
+// var person = {
+//   firstName: 'Alejandro',
+//   lastName: 'Chang',
+//   greet: function() {
+//     console.log('Hello, ' + this.firstName + ' ' + this.lastName);
+//   }
+// }
+
+// person.greet();
+
+// console.log(person['firstName']) // Alejandro 
+
+function Person(firstName, lastName) {
+  this.firstName = firstName;
+  this.lastName = lastName;
 }
 
-person.greet();
+let Alex = new Person('Alejandro', 'Chang');
 
-console.log(person['firstName']) // Alejandro 
+Person.prototype.greet = function() {
+  console.log('Hello ' + this.firstName + ' ' + this.lastName);
+}
+
+Alex.greet();
+
+let Nancy = new Person('Nancy', 'Yang');
+
+Nancy.greet();
--- stackmin.js
+++ stackmin.js
@@ -1,3 +1,54 @@
 // Create a stack that gives you the result of the minimum number in the Stack
 // Do it O(1) - constant time 
 
+class myStack {
+  constructor() {
+    this.data = []; // [4, 2, 6, 1, 1]
+    this.min = []; // [4, 2, 1, 1]
+  }
+
+  push(el) {
+    this.data.push(el);
+    if (!this.min.length) {
+      this.min.push(el);
+    } else if (el <= this.min[this.min.length - 1]) {
+      this.min.push(el);
+    }
+  }
+
+  pop() {
+    let last = this.data.pop();
+    if (last === this.minimum()) {
+      this.min.pop();
+    }
+
+    return last;
+  }
+
+  minimum() {
+    return this.min[this.min.length - 1];
+  }
+
+  all() {
+    return this.min;
+  }
+}
+
+const first = new myStack;
+first.push(4);
+first.push(2);
+first.push(1);
+first.push(6);
+first.push(1);
+
+first.pop(); 
+
+console.log(first.minimum()); // 1
+console.log(first.all()) // this.min = [ 4, 2, 1 ]
+
+
+//pseudo code
+// first create an array to store every less number you encounter
+// if number is less than last number add to stack
+// if you pop and the number is the least number pop that number as well
+// return last number of min array
