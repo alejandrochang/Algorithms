072a0b78cd3a40d1d69159e08147b40e6c35a17d
Alejandro <alejandrochang10@gmail.com>
2019-01-14 22:25:39 -0800

    string rotation solution

---
+++ CTCI-JS/1.9-stringRotation.js
@@ -0,0 +1,13 @@
+
+
+function stringRotation(str1, str2) {
+  if (str1.length !== str2.length) {
+    return false;
+  }
+
+  return (str2 + str2).includes(str1);
+}
+
+console.log(stringRotation("waterbottle", "erbottlewat")) //  true
+console.log(stringRotation("waterbottle", "erbotlewatt")) //  false
+console.log(stringRotation("aaata", "aataa")) //  true
