de5f38f6747c52e1a2bee0fa2b6d44db372220a1
Alejandro <alejandrochang10@gmail.com>
2019-01-14 17:58:56 -0800

    new folder for ctci-js and two sum completed

---
+++ twosum.js
@@ -0,0 +1,24 @@
+let arr1 = [1, 2, 3, 9]; // false
+let arr2 = [1, 2, 4, 4]; // true
+let arr3 = [-1, 2, 11, 4]; // true
+
+function twoSum(arr, sum) {
+  let numMap = {};
+
+  for (let num of arr) {
+    let sumMinusEl = sum - num; // 10 - -1
+
+    if (numMap[sumMinusEl] === true) {
+      return true;
+    }
+    console.log(numMap);
+
+    numMap[sumMinusEl] = true;
+  }
+
+  return false;
+}
+
+// console.log(twoSum(arr1, 8)); // false
+// console.log(twoSum(arr2, 8)); // true
+console.log(twoSum(arr3, 10)); // true
