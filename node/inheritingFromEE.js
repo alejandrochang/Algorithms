7786b5708f90cd12a114db584e58b0e286fd4493
Alejandro <alejandrochang10@gmail.com>
2019-01-15 11:48:50 -0800

    inherting from the event emitter

---
+++ node/inheritingFromEE.js
@@ -0,0 +1,30 @@
+var EventEmitter = require('events');
+var util = require('util');
+
+
+function Greetr() {
+  this.greeting = 'Hello';
+}
+
+util.inherits(Greetr, EventEmitter);
+
+Greetr.prototype.greet = function(data) {
+  console.log(this.greeting + ' ' + data);
+  this.emit('greet', data); // the mit method, second paramater is data that can be passed 
+}
+
+var greeter1 = new Greetr();
+
+greeter1.on('greet', (data) => {
+  console.log('Inside the Event Emitter methods' + ' my name is:' + data)
+});
+
+greeter1.greet('Alejandro');
+
+
+
+
+
+
+// // Any objects created with greetr should also have access to prototype property of the Event Emitter giving
+// // it access to its methods and properties. 
