c0b16c32e58d4daecdbf8ea271876e76e3fe2e66
Alejandro <alejandrochang10@gmail.com>
2019-01-20 18:13:59 -0800

    progress on p4 and linkedlist with stack

--- PIE/LinkedList/stackImplementation.js
+++ PIE/LinkedList/stackImplementation.js
@@ -4,3 +4,10 @@ a dynamic array and justify your decision. Design the interface to your stack to
 consistent and easy to use.
 */
 
+// Pros and Cons - Dynamic Array
+// O(1) insert, O(1) - access, 
+// cons: hard to sort/rearrange, 
+// O(n) to iterate
+
+// Pros and Cons - Linked List
+// iteratin
