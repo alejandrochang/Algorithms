d7aa1035698b7f726f9c6922cc7b2ecd5a58e876
Alejandro <alejandrochang10@gmail.com>
2019-01-22 20:09:53 -0800

    working solution without punctuations reverseWords

--- PIE/Arrays-Strings/reverseWords.js
+++ PIE/Arrays-Strings/reverseWords.js
@@ -2,8 +2,14 @@
 // Assume that all words are space delimited and treat punctuation the same as letters
 
 function reverseWords(str) {
+  let words = str.split(' ');
+  let result = [];
+  for (let i = words.length - 1; i >= 0; i--) {
+    result.push(words[i]);
+  }
 
+  return result.join(' ');
 }
 
-console.log(reverseWords("Do or do not, there is no try.")); // 'try, no is there not, do or Do
-console.log(reverseWords('.fast is racecar A')); // A racecar is fast
+console.log(reverseWords("Do or do not, there is no try")); // 'try, no is there not, do or Do
+console.log(reverseWords("A racecar is fast")); // A racecar is fast
