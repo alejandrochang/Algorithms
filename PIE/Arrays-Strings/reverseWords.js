9b894ac65f5f140a8d1f45389e5e0273e60902bf
Alejandro <alejandrochang10@gmail.com>
2019-01-22 20:03:17 -0800

    reverse words problem

---
+++ PIE/Arrays-Strings/reverseWords.js
@@ -0,0 +1,9 @@
+// Your function should reverse words in a string
+// Assume that all words are space delimited and treat punctuation the same as letters
+
+function reverseWords(str) {
+
+}
+
+console.log(reverseWords("Do or do not, there is no try.")); // 'try, no is there not, do or Do
+console.log(reverseWords('.fast is racecar A')); // A racecar is fast
