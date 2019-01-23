ae0e2a2bb9bdaeaf822c729e1c7738111cd08231
Alejandro <alejandrochang10@gmail.com>
2019-01-22 19:59:11 -0800

    removeSpecifiedChars more efficient solution

--- PIE/Arrays-Strings/removeSpecifiedChars.js
+++ PIE/Arrays-Strings/removeSpecifiedChars.js
@@ -4,11 +4,20 @@
 // Write an effiecnt function that deletes characters from an ASCII string. 
 // Where any character existing in remove must be deleted from the str.
 
+// Time Complexity O(n + m)
+// space: O(n) - due to the length of the str and how many characters are erased and added to result
 
 function removeSpecifiedChars(str, deleteStr) {
+  let charMap = {};
   let result = '';
+
+  for (let ch of deleteStr) {
+    charMap[ch] = ch;
+  }
+
+  // console.log(charMap);
   for (let ch of str) {
-    if (!deleteStr.includes(ch)) {
+    if (charMap[ch] !== ch) {
       result += ch;
     }
   }
@@ -18,9 +27,10 @@ function removeSpecifiedChars(str, deleteStr) {
 
 
 console.log(removeSpecifiedChars('Battle of the Vowels: Hawaii vs. Gronzy', 'aeiou')); // 'Bttl f th Vwls: Hw vs. Grzny
+// Bttl f th Vwls: Hw vs. Grnzy
 
 
-// O(n^2) solution
+// O(n * m) solution
 // function removeSpecifiedChars(str, deleteStr) {
 //   let result = "";
 //   for (let ch of str) {
