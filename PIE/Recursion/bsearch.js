9d16de73715020d0bdbe9ca00f86f4240bf865b6
Alejandro <alejandrochang10@gmail.com>
2019-01-25 12:10:21 -0800

    bsearch recursion

---
+++ PIE/Recursion/bsearch.js
@@ -0,0 +1,23 @@
+
+// Implement binary search recursively
+
+function bsearch(arr, target) {
+  if (arr.length === 0) {
+    return null;
+  }
+
+  const mid = Math.floor(arr.length / 2);
+  if (arr[mid] === target) {
+    return mid;
+  } else if (arr[mid] > target) {
+    return bsearch(arr.slice(0, mid), target) 
+  } else if (arr[mid] < target) {
+    let result = bsearch(arr.slice(mid + 1), target);
+    return result === null ? null : result + mid + 1;
+  }
+}
+
+
+console.log(bsearch([1, 2, 3, 4, 5, 6, 7, 8, 9], 7)); // 6
+console.log(bsearch([1, 2, 3, 4, 5, 6, 7, 8, 9], 5)); // 4
+console.log(bsearch([1, 2, 3, 4, 5, 6, 7, 8, 9], 1)); // 0
--- PIE/Recursion/factorial.js
+++ PIE/Recursion/factorial.js
@@ -11,6 +11,23 @@ function factorial(n) {
 
 
 
-console.log(factorial(1)); // 1
-console.log(factorial(3)); // 6
-console.log(factorial(4)); // 24
+// console.log(factorial(1)); // 1
+// console.log(factorial(3)); // 6
+// console.log(factorial(4)); // 24
+
+function helper(n) {
+  let result = [];
+
+}
+
+function factorialArr(n) {
+  if (n === 1) {
+    return n;
+  }
+
+  return n * factorial(n - 1);
+}
+
+console.log(factorialArr(1)); // [1]
+console.log(factorialArr(3)); // [1, 2, 3]
+console.log(factorialArr(4)); // [1, 2, 3, 4]
