9ad57471773abafc456e031eaf7358f03bb12c15
Alejandro <alejandrochang10@gmail.com>
2019-01-28 14:19:42 -0800

    manual routing without express

--- node/63-outputing-json/app.js
+++ node/63-outputing-json/app.js
@@ -6,7 +6,7 @@ http.createServer(function(req, res) {
   res.writeHead(200, { 'Content-Type': 'application/json'});
 
   if (req.url === '/') {
-    res.writeHead(200, { 'Content-Type': 'text/html' })
+    // res.writeHead(200, { 'Content-Type': 'text/html' })
     fs.createReadStream(__dirname + '/index.htm').pipe(res);
   } else if (req.url === '/api') {
     var obj = {
