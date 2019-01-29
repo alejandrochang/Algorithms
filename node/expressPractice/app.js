fa0abf7834ac251a610a5c01f4fefcf72e111c69
Alejandro <alejandrochang10@gmail.com>
2019-01-28 17:15:27 -0800

    using middleware and querying understanding

--- node/expressPractice/app.js
+++ node/expressPractice/app.js
@@ -1,32 +1,40 @@
 var express = require('express');
 var app = express();
-var port = process.env.PORT || 3000; // environment variable or something else 
 
+var port = process.env.PORT || 3000; // will be to the nevironment var and if not port 3000
 
-app.use('/', function(req, res, next) {
-  console.log('Request url: ' + req.url);
+app.use('/assets', express.static(__dirname + '/public')); // middleware for static files
+
+app.use('/', (req, res, next) => { // middleware 
+  console.log('Req: ' + req.url);
   next();
-})
+}); 
 
-app.get('/', function(req, res) {
-  res.send('<html><head></head><body><h1>Hello World 2.0</h1></body></html>');
+app.get('/', (req, res) => {
+  res.send('<html><head><link href=assets/style.css type=text/css rel=stylesheet /></head><body><h1>Hello World</h1></body></html>')
 });
 
-app.get('/person/:id', function(req, res) {
-  res.send('<html><head></head><body><h1>Person: ' + req.params.id + '</h1></body></html>');
+app.get('/users/:userId/books/:bookId', (req, res) => {
+  res.send('<html><head></head><body><h1> UserId: ' + req.params.userId + ' Book: ' + req.params.bookId + '</h1></body></html>') // pattern matching, dealing with routes 
 });
 
-app.get('/api', function(req, res) {
-  res.json({firstName: 'Alex', lastName: 'Chang'});
+app.get('/json', (req, res) => {
+  res.json({
+    firstName: 'Alex',
+    lastName: 'Chang',
+    address: {
+      state: 'New York',
+      city: 'New York'
+    }
+  });
 });
+
+// app.get(/p/, function(req, res) { // matches with anything p 
+//   res.send("<html><head></head><body><h1>Matching anything with P</h1></body></html>");
+// });
+
+
+
 
 app.listen(port);
-
-
-// Enabling CORS 
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
 
