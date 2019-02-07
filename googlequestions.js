f67df2c50f7cc556251c981991519f74ca91cf01
Alejandro <alejandrochang10@gmail.com>
2019-02-07 00:02:35 -0800

    folder structure for the data structures

---
+++ googlequestions.js
@@ -0,0 +1,27 @@
+// 1  a) You want to make a social network, choose data structure on how to represent data and design algorithm to find friend of friends
+//    b) suppose you want to find top k friend recommendation, and its ranked by number of mutual friends you have with said friend of friends, 
+//      how would you do this ?
+
+//   2) How would you find the longest text in a dom using JS ?
+
+//   3) Design a search auto complete, the context is you have to search for cities.
+      // a) Design structure of state, 
+      // b) Discuss how you would write it on front end, how often the queries should be ran, write up js for the search feature, 
+      // c) What data structure you would use for back end d) code up the part where how you would do the look up for top k cities
+
+// 4) https://leetcode.com/problems/dungeon-game/
+
+// 5) a) Given a knight on an infinite sized board, and you start at cell(0, 0), given a function that passes in (x, y), 
+ // find out minimum number of steps it would take to get from(0, 0) to(x, y)
+
+// b) What if the function also passes in a list of squares that are blocked, how would you change the original function?
+
+
+// LeetCode
+// Dungeon Game - LeetCode
+// Level up your coding skills and quickly land a job.This is the best place to expand your knowledge and get prepared for your next interview.
+//   LeetCode
+// Dungeon Game - LeetCode
+// Level up your coding skills and quickly land a job.This is the best place to expand your knowledge and get prepared for your next interview.
+//   https://leetcode.com/problems/dungeon-game/
+
