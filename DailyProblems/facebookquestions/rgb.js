da629f900aea7108bd61387e11c807b9d36ae25a
Alejandro Chang <alejandrochang10@gmail.com>
2019-02-27 23:20:45 -0800

    two new questions

---
+++ DailyProblems/facebookquestions/rgb.js
@@ -0,0 +1,15 @@
+// Given an array of strictly the characters 'R', 'G', and 'B', 
+// segregate the values of the array so that all the Rs come first, 
+// the Gs come second, and the Bs come last. You can only swap elements of the array.
+
+// Do this in linear time and in-place. O(n) * O(n) space
+
+// For example, given the array ['G', 'B', 'R', 'R', 'B', 'R', 'G'], 
+// it should become ['R', 'R', 'R', 'G', 'G', 'B', 'B'].
+
+function rgb(array){
+
+}
+
+
+console.log(rgb(['G', 'B', 'R', 'R', 'B', 'R', 'G'])); // ['R', 'R', 'R', 'G', 'G', 'B', 'B']
