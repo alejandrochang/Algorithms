cab6362f0a2e8e9c38abdbc39c0097d4a3284918
Alejandro Chang <alejandrochang10@gmail.com>
2019-03-04 08:56:46 -0800

    3and1 naive solution

---
+++ DailyProblems/googlequestions/3and1.js
@@ -0,0 +1,29 @@
+
+// Given an array of integers where every integer occurs 
+// three times except for one integer, which only occurs once,
+//  find and return the non-duplicated integer.
+
+// For example, given [6, 1, 3, 3, 3, 6, 6], return 1. Given [13, 19, 13, 13], return 19.
+// Do this in O(N) time and O(1) space.
+
+
+// Do this in O(N) time and O(1) space.
+
+function repeatedOnlyOnce(array) {
+  let charMap = {};
+
+  for (let num of array) {
+    charMap[num] = charMap[num] + 1 | 1;
+  }
+
+  for (let num in charMap) {
+    if (charMap[num] === 1) {
+      return num;
+    }
+  }
+
+  return null;
+}
+
+console.log(repeatedOnlyOnce([6, 1, 3, 3, 3, 6, 6])); // 1
+console.log(repeatedOnlyOnce([13, 19, 13, 13])); // 19
