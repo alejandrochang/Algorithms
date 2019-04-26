4f6844d8da59ccf12c05dc37a315554322b9b602
Alejandro Chang <alejandrochang10@gmail.com>
2019-04-25 22:52:25 -0700

    adding new sub conseq problem

---
+++ DailyProblems/microsoftquestions/longestConseSeq.js
@@ -0,0 +1,31 @@
+// Given an unsorted array of integers, find the length of the 
+// longest consecutive elements sequence.
+
+
+// For example given [100, 4, 299, 1, 3,  2] -> [1, 2, 3, 4]
+
+
+function longestConsecutiveSequence(arr) {
+  let hashMap = {};
+
+  for (let num of arr) {
+    hashMap[num] = [num];
+  }
+
+  console.log(hashMap);
+}
+
+
+longestConsecutiveSequence([100, 4, 200, 1, 3, 2]); // [1, 2, 3, 4]
+
+// Time Constrain : O(n) complexity
+
+
+// Input: array
+// Output: array
+// Constraints: O(n) Time
+
+
+// we know that we have to iterate store these in some sort of data format to be able to see if there is every consective numbers. 
+// iterate through all put them in a hashMap of itself inside an array
+// iterate again see if any are less or greater than if they are add them appropriately
