4f07a1f635aa034955c0bf3793541db79362a4d8
Alejandro Chang <alejandrochang10@gmail.com>
2019-05-25 22:32:35 -0700

    square root solution

---
+++ squareRoot.js
@@ -0,0 +1,7 @@
+// Given a real number n, find the square root of n. For example, given n = 9, return 3
+
+const squareRoot = (num) => {
+  return Math.sqrt(num);
+}
+
+console.log(squareRoot(81)); // 9
