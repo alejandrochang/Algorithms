e14ea50b94e9dd579ec8362ff65602115c905ff3
Alejandro Chang <alejandrochang10@gmail.com>
2019-07-05 09:26:25 -0700

    more concise solutiona

--- MasterCodingInterview/commonItems.js
+++ MasterCodingInterview/commonItems.js
@@ -1,23 +1,36 @@
 // Given 2 arrays, create a function that lets a user know (true/false)
 // whether these two arrays contain any common items
 
-
-const commonItems = (arr, arr2) => {
-  let countMap = {};
-  for (let ch of arr) {
-    countMap[ch] = countMap[ch] + 1 || 1;
+function commonItems(arr1, arr2) {
+  let map = {};
+  for (let ch of arr1) {
+    if (!map[ch]) {
+      map[ch] = true;
+    }
   }
 
   for (let ch of arr2) {
-    countMap[ch] = countMap[ch] + 1 || 1;
-  }
-
-  for (let key in countMap) {
-    if (countMap[key] === 2) return true;
+    if (map[ch]) return true;
   }
 
   return false;
 }
+// const commonItems = (arr, arr2) => {
+//   let countMap = {};
+//   for (let ch of arr) {
+//     countMap[ch] = countMap[ch] + 1 || 1;
+//   }
+
+//   for (let ch of arr2) {
+//     countMap[ch] = countMap[ch] + 1 || 1;
+//   }
+
+//   for (let key in countMap) {
+//     if (countMap[key] === 2) return true;
+//   }
+
+//   return false;
+// }
 
 
 const a = ['a', 'b', 'c', 'x'];
@@ -30,7 +43,7 @@ console.log(commonItems(a, c)); // true
 // i: array
 // o: boolean
 // no constraints
-// O(n) - iterating
+// O(a + b) - iterating
 
-// 1) brute force/naive approach, whys is it inefficient?
-// 2) 
+// 1) brute force/naive approach, whys is it inefficient? - O(n^2)
+// 2) Theres 2 different arrays meaning that it is a*b or a + b
