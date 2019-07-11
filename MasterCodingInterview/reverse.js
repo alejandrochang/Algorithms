81e422291857cab7d77e4b644f916cd3ba491999
Alejandro Chang <alejandrochang10@gmail.com>
2019-07-11 09:10:41 -0700

    finalizing reverse code

--- MasterCodingInterview/reverse.js
+++ MasterCodingInterview/reverse.js
@@ -1,16 +1,20 @@
 function reverse(str) {
-  return str.split(' ').reverse().join(' ');
+  if (!str || str.length < 2 || typeof str !== 'string') return 'Incorrect input, please make sure its a string';
+  return str.split('').reverse().join('');
 }
 
 
 function reverse2(str){
   let result = '';
-  for (let ch of str) {
-    result += ch;
+  for (let ch of str.split('')) {
+    result = ch + result;
   }
+
+  return result;
 }
 
-console.log(reverse('racecar'))
-console.log(reverse('hello world'))
-console.log(reverse('now! hey '))
-console.log(reverse('Hi! My name is Alejandro. '))
+console.log(reverse('racecar'));
+console.log(reverse('hello world'));
+console.log(reverse('now! hey'));
+console.log(reverse('Hi My name is Andrei.'));
+console.log(reverse2('Hi My name is Andrei.'));
