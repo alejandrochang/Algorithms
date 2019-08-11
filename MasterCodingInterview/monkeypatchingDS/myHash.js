7cabdc872f81aebf058e30660af954dc25e24be2
Alejandro Chang <alejandrochang10@gmail.com>
2019-08-11 16:24:36 -0700

    finalizing monkeypatching hash

--- MasterCodingInterview/monkeypatchingDS/myHash.js
+++ MasterCodingInterview/monkeypatchingDS/myHash.js
@@ -5,7 +5,7 @@ class Hash {
 
   _hash(key) {
     let hash = 0;
-    for (let i = 0; i < ket.length; i++) {
+    for (let i = 0; i < key.length; i++) {
       hash = (hash + key.charCodeAt(i) * i) % this.data.length;
     }
 
@@ -13,19 +13,26 @@ class Hash {
   }
 
   set(key, value) {
-    this.data.push([key, value]);
+    let address = this._hash(key);
+    if (!this.data[address]) {
+      this.data[address] = [];
+    }
+
+    this.data[address].push([key, value]);
+    return this.data;
   }
 
   get(key) {
-    for (let i = 0; i < this.data.length; i++) {
-      if (this.data[i][0] === key) return this.data[i][1];
-    }
+    let address = this._hash(key);
+    const currentBucket = this.data[address];
+    if (!currentBucket.length) return null;
 
-    return null;
+    return currentBucket[0][1];
   }
 }
 
 const myHash = new Hash(50);
 myHash.set('grapes', 10000);
-console.log({ myHash });
-// myHash.get('grapes'); // 100000
+console.log(myHash.get('grapes')); // 100000
+
+// _hash is common practice for javascript private functions (developer standard)
