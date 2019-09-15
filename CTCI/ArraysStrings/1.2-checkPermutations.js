550a8bf8514694b892ae5e42983180f5df8e4459
Alejandro Chang <alejandrochang10@gmail.com>
2019-09-15 14:36:43 -0700

    setting up folder structure for problems to be completed

---
+++ CTCI/ArraysStrings/1.2-checkPermutations.js
@@ -0,0 +1,8 @@
+// Check Permutations: Given two strings, write a method to decide if one is a permutation of the other. 
+// ex. 'abc', 'cba' // true
+
+const permutations = () => {
+
+}
+
+console.log(permutations()); //
