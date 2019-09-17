ad0fb86f4deb28e919c61950903456d9185682b3
Alejandro Chang <alejandrochang10@gmail.com>
2019-09-16 21:18:28 -0700

    adding palindrom permutation problem

---
+++ CTCI/ArraysStrings/1.4-palindromePermuation.js
@@ -0,0 +1,12 @@
+// Palindrome Permutation
+
+// Given a string write a function to check if its a permutation of a palindrome.
+// A palindrome is a word or phrase that is the same forwards and backwards. A permutation is a rearrangement of letters.
+// The palindrome does not need to be lmiited to just dictionary words. 
+
+// palindrome ex. racecar -> racecar
+// permutation ex. ab -> ab, ba
+
+const palindromPermutations = () => {
+
+}
