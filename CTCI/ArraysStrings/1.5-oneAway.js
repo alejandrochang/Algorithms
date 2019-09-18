3f1b936c40e5cc6c5df289c59c9ba29f9719dfef
Alejandro Chang <alejandrochang10@gmail.com>
2019-09-18 07:02:22 -0700

    adding oneaway problem

---
+++ CTCI/ArraysStrings/1.5-oneAway.js
@@ -0,0 +1,16 @@
+// 1.5 OneAway
+// There are three types of edits that can be performed on strings, insert a character,
+// remove a character an d replace a character.
+// Given two strings, write a function to check if they are one edit (or zero edits) away
+
+const OneAway = (str) => {
+
+}
+
+
+// console.log(OneAway());
+
+// I: str
+// O: boolean
+// time:
+// space:
