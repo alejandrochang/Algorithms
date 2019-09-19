25bf17d5b14c948a8925ad5ef91c6918703f3c6e
Alejandro Chang <alejandrochang10@gmail.com>
2019-09-19 08:45:15 -0700

    progress on oneaway problem

--- CTCI/ArraysStrings/1.5-oneAway.js
+++ CTCI/ArraysStrings/1.5-oneAway.js
@@ -1,16 +1,39 @@
 // 1.5 OneAway
 // There are three types of edits that can be performed on strings, insert a character,
-// remove a character an d replace a character.
+// remove a character and replace a character.
 // Given two strings, write a function to check if they are one edit (or zero edits) away
 
-const OneAway = (str) => {
+const OneAway = (str1, str2) => {
+  // let charMap = {};
+  // let count = 0;
 
+  // for (let ch of str1) {
+  //   charMap[ch] + 1 || 1;
+  // }
+
+  // for (let ch of str2) {
+  //   if (charMap[ch]) {
+  //     delete charMap[ch];
+  //   } else {
+  //     count++;
+  //   }
+  // }
+
+  // return count < 2;
 }
 
 
-// console.log(OneAway());
+console.log(OneAway('pale', 'ple')); // true
+console.log(OneAway('pale', 'pale')); // true
+console.log(OneAway('pale', 'bale')); // true
+console.log(OneAway('pale', 'bake')); // false
 
 // I: str
 // O: boolean
 // time:
 // space:
+
+
+// pseudo:
+// keep count of all the chars put them in charMap, remove ones already there
+// if the leftovers is greater > 1, we know its more than one away. 
