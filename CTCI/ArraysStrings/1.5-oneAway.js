e1f3d6cebe828b5589bd6d9cfa8c62b5a3e2b2e5
Alejandro Chang <alejandrochang10@gmail.com>
2019-09-19 09:05:02 -0700

    solution to one away

--- CTCI/ArraysStrings/1.5-oneAway.js
+++ CTCI/ArraysStrings/1.5-oneAway.js
@@ -4,24 +4,26 @@
 // Given two strings, write a function to check if they are one edit (or zero edits) away
 
 const OneAway = (str1, str2) => {
-  // let charMap = {};
-  // let count = 0;
+  if (Math.abs(str1.length - str2.length) >= 2) return false;
 
-  // for (let ch of str1) {
-  //   charMap[ch] + 1 || 1;
-  // }
+  let countMap1 = {};
+  let count = 0;
 
-  // for (let ch of str2) {
-  //   if (charMap[ch]) {
-  //     delete charMap[ch];
-  //   } else {
-  //     count++;
-  //   }
-  // }
+  for (let ch of str1) {
+    countMap1[ch] = countMap1[ch] + 1 || 1;
+  }
 
-  // return count < 2;
+  for (let ch of str2) {
+    if (countMap1[ch]) {
+      delete countMap1[ch];
+    }
+  }
+
+  return Object.keys(countMap1).length <= 1;
 }
 
+// { a: 2, b:1 }
+// { a: 2, b:1, c: 1}
 
 console.log(OneAway('pale', 'ple')); // true
 console.log(OneAway('pale', 'pale')); // true
@@ -30,8 +32,8 @@ console.log(OneAway('pale', 'bake')); // false
 
 // I: str
 // O: boolean
-// time:
-// space:
+// time: O(n)
+// space: O(n)
 
 
 // pseudo:
