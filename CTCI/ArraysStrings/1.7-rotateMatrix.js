362642f21fb70cfab1151dd9745ec648cc480752
Alejandro Chang <alejandrochang10@gmail.com>
2019-09-22 15:26:59 -0700

    adding rotating matrix problem

---
+++ CTCI/ArraysStrings/1.7-rotateMatrix.js
@@ -0,0 +1,11 @@
+// Rotate Matrix
+// Given an image represented by a N x N matrix,
+// where eaxch pixel in the image is 4 bytes, write a method to rotate the image by 90 degrees?
+// Can you do this in place?
+
+const rotateMatrix = (arr) => {
+
+}
+
+
+console.log(rotateMatrix(arr));
