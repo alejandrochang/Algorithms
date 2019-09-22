f9191b2ad8f145760e5a2742359687264cf1e898
Alejandro Chang <alejandrochang10@gmail.com>
2019-09-22 15:19:23 -0700

    solution for compressed string

--- CTCI/ArraysStrings/1.6-stringCompression.js
+++ CTCI/ArraysStrings/1.6-stringCompression.js
@@ -6,13 +6,30 @@
 // You can assume the string has only uppercase and lowercase letters. 
 
 const stringCompression = (str) => {
-  
+  let compressedString = '';
+  let count = 1;
+
+  for (let i = 0; i < str.length; i++) {
+    if (str[i + 1] === str[i]) {
+      count++;
+    } else {
+      compressedString += (str[i] + count.toString());
+      count = 1;
+    }
+  }
+
+  return compressedString.length < str.length ? compressedString : str;
 }
 
 
-console.log(stringCompression(aabccccccaaa)); //  a2b1c5a3
+console.log(stringCompression('aabcccccaaa')); //  a2b1c5a3
 
 // I: String
 // O: String (either compressed or original)
-// Time:
-// Space:
+// Time: O(n)
+// Space: O(n)
+
+// Where n is the length of the string
+
+// pseudo code:
+// Keep count of the compressedString and compare it to its next idx position
