6db3fe68652f0da4039ab40d31c071fd50bd038c
Alejandro Chang <alejandrochang10@gmail.com>
2019-09-22 15:06:01 -0700

    setting up string compression problem

---
+++ CTCI/ArraysStrings/1.6-stringCompression.js
@@ -0,0 +1,18 @@
+// String Compression
+
+// Implement a method to perform basic string compression using the counts of repeated characters
+// For example: the string aabccccccaaa would become a2b1c5a3. If the 'compressed' string would 
+// not become smaller than the original string, your method should return the original string.
+// You can assume the string has only uppercase and lowercase letters. 
+
+const stringCompression = (str) => {
+  
+}
+
+
+console.log(stringCompression(aabccccccaaa)); //  a2b1c5a3
+
+// I: String
+// O: String (either compressed or original)
+// Time:
+// Space:
