0254f82e199a7b15fc8cbda0fe92d67059aaf820
Alejandro Chang <alejandrochang10@gmail.com>
2019-09-29 16:36:03 -0700

    rotating matrix solution in place

--- CTCI/ArraysStrings/1.7-rotateMatrix.js
+++ CTCI/ArraysStrings/1.7-rotateMatrix.js
@@ -3,21 +3,43 @@
 // where eaxch pixel in the image is 4 bytes, write a method to rotate the image by 90 degrees?
 // Can you do this in place?
 
-const rotateMatrix = (arr) => {
-  let result = ;
-  for (let i = 0; i < arr.length; i++) {
-    result[i + 2] = arr[i];
+const rotateMatrix = (m) => {
+  let n = m.length;
+
+  for (let i = 0; i < Math.floor(n / 2); i++) {
+    for (let j = 0; j < n - (2 * i) - 1; j++) {
+      let t = m[i + j][n - 1 - i];
+      m[i + j][n - 1 - i] = m[i][i + j];
+      m[i][i + j] = t;
+
+      t = m[n - 1 - i][n - 1 - i - j];
+      m[n - 1 - i][n - 1 - i - j] = m[i][i + j];
+      m[i][i + j] = t;
+
+      t = m[n - 1 - i - j][i];
+      m[n - 1 - i - j][i] = m[i][i + j];
+      m[i][i + j] = t;
+    }
   }
+  return m;
+};
 
-  return result;
-}
-
+// I: matrix nxn 
+// O: rotated matrix 90 degrees (clockwise)
+// Constraints: rotate matrix in place, optimize
+// Edge case: empty matrix, even and odd values for n
 
 // 1 2 3       7 4 1      0 1 2
 // 4 5 6   ->  8 5 2 -->  3 4 5 
 // 7 8 9       9 6 3      6  7 8 
 
 
-const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9 ];
-
-console.log(rotateMatrix(arr));
+  console.log(rotateMatrix(JSON.stringify([[1, 2], [3, 4]])));
+  // console.log(rotateMatrix([[1, 2, 3], [4, 5, 6], [7, 8, 9]]), [[7, 4, 1], [8, 5, 2], [9, 6, 3]]);
+  // console.log(rotateMatrix([[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 16]]),
+  //                         [[13, 9, 5, 1], [14, 10, 6, 2], [15, 11, 7, 3], [16, 12, 8, 4]]);
+  // console.log(rotateMatrix([[1, 2, 3, 4, 5], [6, 7, 8, 9, 10], [11, 12, 13, 14, 15], [16, 17, 18, 19, 20], [21, 22, 23, 24, 25]]), 
+  //                         [[21, 16, 11, 6, 1], [22, 17, 12, 7, 2], [23, 18, 13, 8, 3], [24, 19, 14, 9, 4], [25, 20, 15, 10, 5]]);
+  // console.log(rotateMatrix([]), []),
+  console.log(rotateMatrix([[]])),
+  console.log(rotateMatrix([[1]]))
