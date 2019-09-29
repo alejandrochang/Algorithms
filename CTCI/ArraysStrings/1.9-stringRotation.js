1e34d7b520db011ec420d10a2a4d84676d5258e5
Alejandro Chang <alejandrochang10@gmail.com>
2019-09-28 20:08:41 -0700

    string rotations solutions

--- CTCI/ArraysStrings/1.9-stringRotation.js
+++ CTCI/ArraysStrings/1.9-stringRotation.js
@@ -11,4 +11,13 @@ const stringRotation = (s1, s2) => {
 const s1 = 'waterboottle';
 const s2 = 'erbottlewat';
 
-console.log(stringRotation(s1, s2)); // true or false
+console.log(stringRotation(s1, s2)); // true
+
+// I: string, string
+// O: boolean
+// Time;
+// Space:
+
+// Pseudo:
+// grab 1st string and 2string, sort both of them,
+
