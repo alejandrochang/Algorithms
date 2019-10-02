f669b86557eff959879b67fef6e400cc9adb3f7d
Alejandro Chang <alejandrochang10@gmail.com>
2019-10-01 19:54:52 -0700

    processing zero matrix

--- CTCI/ArraysStrings/1.8-zeroMatrix.js
+++ CTCI/ArraysStrings/1.8-zeroMatrix.js
@@ -3,9 +3,32 @@
 // its entire row and column are set to 0.
 
 
-const zeroMatrix = (arr) {
-
+const zeroMatrix = (arr) => {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length; j++) {
+      if (arr[i][j] === 0) {
+        // if equals zero call helper functions
+    }
+  }
 }
 
+// [row][col]
 
-// console.log(zeroMatrix());
+const A = [
+  [1, 2, 3, 4],
+  [5, 6, 7, 8],
+  [9, 10, 11, 12],
+  [13, 14, 15, 16]
+]
+
+const F = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9]
+]
+
+console.log(zeroMatrix(A));
+console.log(zeroMatrix(F));
+
+// iterate through two for loops
+// get to the inside o
