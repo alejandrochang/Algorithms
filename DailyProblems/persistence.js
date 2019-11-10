a06107d309c8325084d99f323c8f458b3da6fb50
Alejandro Chang <alejandrochang10@gmail.com>
2019-11-10 18:03:52 +0800

    solution for persistence problem

--- DailyProblems/persistence.js
+++ DailyProblems/persistence.js
@@ -1,4 +1,5 @@
 // Persistence 
+// Given a number see how many times it takes to reach a single number if you multiply ever single number out. 
 
 // 1.) persistence(39) === 3
 // 3 * 9 = 27, 2 * 7 = 14, 1 * 4 = 4. We had to multiply 3 times in order to reach a single number. Therefore, the answer is 3.
@@ -6,7 +7,20 @@
 // 9 * 9 * 9 = 729, 7 * 2 * 9 = 126, 1 * 2 * 6 = 12, 1 * 2 = 2. Four calculations to reach a single number.
 
 const persistence = (num) => {
-  
+  // turn to array, keep count and total
+  let numberArray = num.toString().split('');
+  let count = 0;
+
+  while (numberArray.length > 1) {
+    let total = numberArray.reduce((sum, currentValue) => {
+      return sum * currentValue;
+    });
+
+    numberArray = total.toString().split('');
+    count++;
+  }
+
+  return count;
 }
 
 console.log(persistence(39)); // === 3
@@ -20,3 +34,4 @@ console.log(persistence(999)); // === 4
 // [pseudo]
 // turn numbers to string, split into array
 // iterate through the array see if it arrays length is less than one
+// reduce on the total result of the array, split again see if its longer than 1
