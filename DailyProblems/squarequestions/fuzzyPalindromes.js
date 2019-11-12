8692701ea05032426bda97f9d675a6ee31b6800e
Alejandro Chang <alejandrochang10@gmail.com>
2019-11-12 09:33:10 +0800

    adding fuzzy palindrome problems

---
+++ DailyProblems/squarequestions/fuzzyPalindromes.js
@@ -0,0 +1,8 @@
+// Write a function that returns whether or not two strings
+// are fuzzy palindromes of one another (i.e. ignore spaces and capitalization).  
+
+const fuzzyPalindromes = () => {
+
+}
+
+console.log(fuzzyPalindromes()); // 
