0836ecdaf9643956b2c4911fe75e5c64418158c3
Alejandro Chang <alejandrochang10@gmail.com>
2019-11-12 09:50:27 +0800

    solution for fuzzy palindromes

--- DailyProblems/squarequestions/fuzzyPalindromes.js
+++ DailyProblems/squarequestions/fuzzyPalindromes.js
@@ -1,8 +1,42 @@
 // Write a function that returns whether or not two strings
 // are fuzzy palindromes of one another (i.e. ignore spaces and capitalization).  
 
-const fuzzyPalindromes = () => {
+// Given a string S, Check if characters of the given string can be rearranged to form a palindrome. 
+// For example characters of 'geeksogeeks' can be rearranged to form a palindrome 
+// “geeksoskeeg”, but characters of “geeksforgeeks” cannot be rearranged to form a palindrome.
 
+const fuzzyPalindromes = (str) => {
+  const countMap = {};
+
+  for (let ch of str) {
+    if (countMap[ch]) {
+      // if exists delete
+      delete countMap[ch]
+    } else {
+      countMap[ch] = 1;
+    }
+  }
+
+  for (let ch in countMap) {
+    if (Object.keys(countMap).length > 1) {
+      return false;
+    }
+  }
+
+  return true;
 }
 
-console.log(fuzzyPalindromes()); // 
+console.log(fuzzyPalindromes('geeksogeeks')); // true, geeksoskeeg
+console.log(fuzzyPalindromes('ABCDBCA')); // true, geeksoskeeg
+console.log(fuzzyPalindromes('geeksforgeeks')); // false
+
+
+// I: string
+// O: boolean
+// Time: O(n)
+// Space: O(n)
+
+// [pseudo]
+
+// create a count map that adds and deletes the characters of the string,
+// if there is more than one remaining its false, otherwise its true
