71f23716bf0423e1cb80a3c920e99b22b8ec37e3
Alejandro Chang <alejandrochang10@gmail.com>
2019-11-13 14:45:12 +0800

    further progress on queues multiple implementations

--- Data Structures/Queues/queue.js
+++ Data Structures/Queues/queue.js
@@ -9,21 +9,69 @@
 //     q.remove(); // returns 1;
 
 
+// add front, remove end
 class Queue {
   constructor() {
     this.data = [];
   }
 
   add(el) {
-    this.data.push(el); // we can use unshift
+    // add to end
+    this.data.push(el);
   }
 
   remove() {
-    return this.data.shift(); // pop
+    // Remove from front, O(n)
+    return this.data.shift();
   }
-
 }
 
-const q = new Queue();
-q.add(1);
-console.log(q.remove()); // returns 1;
+
+// methods: add, remove, peek
+// Time Complexity: O(1) - constant for all methods
+
+// Queue: Data Structure can either add in front and remove from end, or add to end and remove from front
+
+class Queue2 {
+  constructor() {
+    this.data = [];
+  }
+
+  add(el) {
+    // add to front, O(n)
+    this.data.unshift(el);
+  }
+
+  remove() {
+    // remove from end
+    return this.data.pop();
+  }
+}
+
+const queue = new Queue;
+const queue2 = new Queue2;
+
+
+queue.add(1)
+queue2.add(1)
+queue.add(10)
+queue2.add(10)
+// queue.add(1000)
+// queue2.add(1000)
+
+queue.remove();
+queue2.remove();
+
+queue.add(100)
+queue2.add(100)
+
+console.log(JSON.stringify(queue, null, 4));
+console.log(JSON.stringify(queue2, null, 4));
+
+// Solution:
+// {
+//     "data": [10, 100]
+// }
+// {
+//     "data": [ 100, 10]
+// }
