6481ad8f612be37030eb4b490e4a0869cea92729
Alejandro Chang <alejandrochang10@gmail.com>
2019-11-15 16:51:35 -0800

    getElementsByClassName and getElementsByTagName

--- DOM/dom.js
+++ DOM/dom.js
@@ -21,14 +21,42 @@
 // Selectors to query the DOM:
 // getElementById
 
-const headerTitle = document.getElementById('header-title');
-const header = document.getElementById('main-header');
+// const headerTitle = document.getElementById('header-title');
+// const header = document.getElementById('main-header');
 
-// headerTitle.textContent = 'Alejandro Chang';
-// headerTitle.innerText = 'Goodbye'
+// // headerTitle.textContent = 'Alejandro Chang';
+// // headerTitle.innerText = 'Goodbye'
 
-// puts the html inside the H1 tag
-headerTitle.innerHTML = '<h3>Substituting title</h3>'
-// styling directly 
-header.style.borderBottom = 'solid 3px red'
+// // puts the html inside the H1 tag
+// headerTitle.innerHTML = '<h3>Substituting title</h3>'
+// // styling directly 
+// header.style.borderBottom = 'solid 3px red'
 
+
+// Get Elements by classname
+// var items = document.getElementsByClassName('list-group-item');
+// console.log({ items });
+// console.log(items[1]);
+
+// items[1].textContent = 'Nahh there'
+// items[1].style.fontWeight = 'bold'
+
+// for (let i = 0; i < items.length; i++) {
+//   items[i].style.backgroundColor = 'grey'
+// }
+
+// items[1].style.backgroundColor = 'blue'
+
+// Get Elements by Tag Name
+var li = document.getElementsByTagName('li');
+console.log({ li });
+console.log(li[1]);
+
+li[1].textContent = 'Nahh there'
+li[1].style.fontWeight = 'bold'
+
+for (let i = 0; i < li.length; i++) {
+  li[i].style.backgroundColor = 'gray'
+}
+
+li[1].style.backgroundColor = 'blue'
--- DOM/dom.md
+++ DOM/dom.md
@@ -21,7 +21,7 @@ Important methods:
   <h1 id="header">Header</h1>
 ```
 
-2) textContent and innerText are basically interchangable. innerText pays attention to styling while textContent does not (cares just about the text)
+2) `textContent` and `innerText` are basically interchangable. innerText pays attention to styling while textContent does not (cares just about the text)
 
 ```js
   const header = document.getElementById('header');
@@ -31,10 +31,40 @@ Important methods:
   header.innertText('Alejandro');
 ```
 
-3) innerHTML inserts HTML into the document, but doesn't get rid of it
+3) `innerHTML` inserts HTML into the document, but doesn't get rid of it
 
 ```js
   header.innerHTML = <h3>Hello World</h3>
 ```
 This is what it ends up loking likle:
 <h1 id="header-title"><h3>Hello World</h3></h1>
+
+
+4) `getElementsByClassName` and `getElementsByTagName`
+
+```html
+<ul id="items" class="list-group">
+  <li class="list-group-item">Item 1</li>
+  <li class="list-group-item">Item 2</li>
+  <li class="list-group-item">Item 3</li>
+  <li class="list-group-item">Item 4</li>
+  <li>Item 5</li>
+</ul>
+```
+
+```js
+  //  By className
+  let items = document.getElementsByClassName('list-group-item');
+  for (let i = 0; i < items.length; i++) {
+    items[i].style.backgroundColor = 'red';
+  }
+```
+
+```js
+  // By Tagname
+  let items = document.getElementsByTagName('li')
+  for (let i = 0; i < items.length; i++) {
+    items[i].style.backgroundColor = 'red';
+  }
+
+```
