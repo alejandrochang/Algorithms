970ae462337d71265aecdd3995131644150091fc
Alejandro Chang <alejandrochang10@gmail.com>
2019-11-19 09:29:20 -0800

    further practice and knowledge of dom

--- DOM/practice/main.js
+++ DOM/practice/main.js
@@ -1,86 +1,87 @@
-// functionality to add, edit, delete items
-// functionality to filter and sort items
+// Functionalities: add, delete, clear all  
+// filter
+
+// Add and element to list:
+// trigger an event on submit
+// get the text thats inputed
+// create a list element and add the text to the element
 
 let form = document.getElementById('addForm');
-let itemList = document.getElementById('items');
+let listItems = document.getElementById('items');
 
 form.addEventListener('submit', addItem);
-itemList.addEventListener('click', removeItem);
 
-// add items to list
 function addItem(e) {
-  // prevent browser submit
+  // prevent brwoser defualt behavior
   e.preventDefault();
 
   let inputText = document.getElementById('input-1').value;
 
-  // create button, add style
+  // add button plus X
   let button = document.createElement('button');
   button.className = 'btn btn-danger btn-sm float-right delete';
   button.appendChild(document.createTextNode('X'));
 
-  // create li add style
+  // add li, buttton to li and text input
   let li = document.createElement('li');
   li.className = 'list-group-item';
-  li.appendChild(document.createTextNode(inputText))
-
+  li.appendChild(document.createTextNode(inputText));
   li.appendChild(button);
-  itemList.appendChild(li);
 
+  listItems.appendChild(li);
   document.getElementById('input-1').value = '';
 }
 
-// remove an item,
-// get all the buttons, place handler there
-// on click event we want remove item from dom
+// remove
+listItems.addEventListener('click', removeItem);
 
 function removeItem(e) {
+  // listItem: e.target
   if (e.target.classList.contains('delete')) {
-    if (confirm('Are you sure?')) {
-      // parentElement is li
-      // let li = e.target.parentElement;
-      itemList.removeChild(e.target.parentElement);
+    if (confirm('You sure you want to delete item?')) {
+      let li = e.target.parentElement;
+      listItems.removeChild(li);
     }
   }
 }
 
 // filter
-let filterInput = document.getElementById('filter');
-filterInput.addEventListener('keyup', filterList);
 
-function filterList(e) {
-  let text = e.target.value.toLowerCase();
-  // get list
-  let items = itemList.getElementsByTagName('li');
 
-  Array.from(items).forEach((item) => {
-    // if found get the text
-    let itemName = item.firstElementChild.textContent;
-    if (itemName.toLowerCase().indexOf(text) !== -1) {
-      item.style.display = 'block';
-    } else {
-      item.style.display = 'none';
-    }
-  });
-}
 
-// clear List:
-// Remove list button id, add event handler on click
-// if click, iterate through list and remove all li's
 
-let clearListButton = document.getElementById('removeList');
-clearListButton.addEventListener('click', removeAllItems);
 
-function removeAllItems(e) {
-  // get itemslist and iterate through them
-  // while (theres a first child) removeChild
-  if (confirm('You sure you want to delete all?')) {
-    while (itemList.firstElementChild) {
-      // itemList.firstElementChild.remove();
-      itemList.removeChild(itemList.firstElementChild);
-    }
-  }
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 // sort list
