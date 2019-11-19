20859e0708cfaf01840fc279724a84373dba3545
Alejandro Chang <alejandrochang10@gmail.com>
2019-11-18 22:38:05 -0800

    second solution

--- InterviewPrep/FB/commonProblems/reverseWords.js
+++ InterviewPrep/FB/commonProblems/reverseWords.js
@@ -1,13 +1,26 @@
 // Given a phrase, reverse the order of the characters of each word.
 
+// const reverseWords = (str) => {
+//   let reverseStr = str.split('').reverse().join('');
+//   return reverseStr.split(' ').reverse().join(' ');
+// }
+
 const reverseWords = (str) => {
-  let reverseStr = str.split('').reverse().join('');
-  return reverseStr.split(' ').reverse().join(' ');
-}  
+  let result = '';
+  let words = str.split(' ');
+
+  for (let i = 0; i < words.length; i++) {
+    let word = i === 0 ? words[i].split('').reverse().join('') : ' ' + words[i].split('').reverse().join('');
+    result += word;
+  }
+
+  return result;
+}
   
 console.log(reverseWords("I love JavaScript!")); // "I evol !tpircSavaJ");
 // I evol !tpircSavaJ
 
 
 // [pseudo]
-// split string into arr, iterate through word
+// split string into arr, reverse, join -> backwards
+// split into space, reverse join into space
