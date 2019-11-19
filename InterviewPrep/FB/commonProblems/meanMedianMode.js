d065a50103313fa3c5b702e319b3fc7234b103c1
Alejandro Chang <alejandrochang10@gmail.com>
2019-11-19 15:54:25 -0800

    mode progres "

--- InterviewPrep/FB/commonProblems/meanMedianMode.js
+++ InterviewPrep/FB/commonProblems/meanMedianMode.js
@@ -18,7 +18,28 @@ const mode = (arr) => {
   }
 }
 
+const median = (arr) => {
+  let countMap = {};
+  let count = 0;
+  let mostRepeated = [];
 
+  for (let int of arr) {
+    countMap[int] = countMap[int] || 1;
+  }
+
+  for (let int in countMap) {
+    if (countMap[int] > count) {
+      count = countMap[int]
+      mostRepeated = [int];
+    } else if (count === countMap[int]) {
+      mostRepeated.push(int);
+    }
+  }
+
+  if (mostRepeated.length === Object.keys(arr).length) return [];
+
+  return mostRepeated
+}
 
 
 
@@ -29,5 +50,5 @@ console.log(mean(arr1)); // 3.43
 console.log(mean(arr2)); // 2.5
 console.log(mode(arr1)); // 4
 console.log(mode(arr2)); // 2.5
-// console.log(median(arr1)); //
-// console.log(median(arr2)); //
+console.log(median(arr1)); // [4, 5]
+console.log(median(arr2)); // []
