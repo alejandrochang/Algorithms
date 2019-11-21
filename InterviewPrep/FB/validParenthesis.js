132d32acf7d5e54757838142884409957f4f9835
Alejandro Chang <alejandrochang10@gmail.com>
2019-11-20 17:19:30 -0800

    more practice

---
+++ InterviewPrep/FB/validParenthesis.js
@@ -0,0 +1,50 @@
+
+// return the string of validParenthesis
+
+// check the validity of the string
+function isValidString(str) {
+  var count = 0;
+  console.log({ str });
+  for (let c of str) {
+    if (c === '(') {
+      count++;
+    } else if (c === ')') {
+      count--;
+    }
+    if (count < 0) return false;
+  }
+  return count === 0;
+}
+
+function removeInvalidParentheses(str) {
+  let levels = [str];
+  console.log({ levels });
+  while (true) {
+    let valid = levels.filter(isValidString);
+    console.log({ valid });
+    if (valid.length > 0) {
+        return new Set(valid);
+    }
+
+    // check validity of all possible substrings with one character removed.
+    let nextlevels = [];
+    for (let str of levels) {
+        for (let i = 0; i < str.length; i++) {
+            nextlevels.push(str.substring(0, i) + str.substring(i + 1));
+        }
+    }
+    levels = nextlevels;
+  }
+  console.log({ levels });
+  return levels;
+}
+
+// let str1 = '(())'; // (())
+// let str2 = '((())'; // (())
+// let str2 = '))(a())'; // (a())
+
+console.log(removeInvalidParentheses(')()(c)(')); //()(c)
+// console.log(removeInvalidParentheses('(a()))'));
+// console.log(removeInvalidParentheses(')('));
+// console.log(removeInvalidParentheses('()))(()'));
+// console.log(removeInvalidParentheses('((str)())'));
