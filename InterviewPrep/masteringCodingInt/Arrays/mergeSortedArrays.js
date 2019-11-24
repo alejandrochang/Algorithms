b2caed6d940f8379aef064a61d6dce97ab9b3dfd
Alejandro Chang <alejandrochang10@gmail.com>
2019-11-24 12:22:49 -0800

    solution for merging arrays

--- InterviewPrep/masteringCodingInt/Arrays/mergeSortedArrays.js
+++ InterviewPrep/masteringCodingInt/Arrays/mergeSortedArrays.js
@@ -1,9 +1,25 @@
 // Merge two sorted arrays
 // I: arr1, arr2
-// O: arr -> sorted 
+// O: arr -> sorted
+// Time: O(n) -> iterating through sorted arrays
+// Space: O(n) -> length of arrays length
 
 const mergeSortedArrays = (arr1, arr2) => {
+  let mergedArr = [];
 
+  while (arr1.length && arr2.length) {
+    if (arr1[0] < arr2[0]) {
+      mergedArr.push(arr1.shift());
+    } else {
+      mergedArr.push(arr2.shift());
+    }
+  }
+
+  return [...mergedArr, ...arr1, ...arr2];
 }
 
 console.log(mergeSortedArrays([0, 3, 4, 31], [4, 6, 30]));
+// [0, 3, 4, 4, 6, 30, 31];
+
+// [pseudo]
+// 
