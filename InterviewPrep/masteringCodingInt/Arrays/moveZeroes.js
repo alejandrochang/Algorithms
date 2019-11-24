a97762747146a221a1998ce4754b23b993cdbe33
Alejandro Chang <alejandrochang10@gmail.com>
2019-11-24 14:26:32 -0800

    moving zeroes solution

--- InterviewPrep/masteringCodingInt/Arrays/moveZeroes.js
+++ InterviewPrep/masteringCodingInt/Arrays/moveZeroes.js
@@ -3,6 +3,7 @@
 // Input: [0,1,0,3,12]
 // Output: [1,3,12,0,0]
 
+// MOVE ZEROES TO FRONT STRATEGY
 const moveZeroes = (arr) => {
   let k = 0;
 
@@ -22,4 +23,5 @@ const arr = [0, 1, 0, 3, 12];
 console.log(moveZeroes(arr)); // [ 1, 3, 12, 0, 0 ]
 
 // iterate through array check to see if curr element is 0
-// if it  is 
+// keep temp variable, swap curr  variable for set variable
+// index into the k element of the arr -> ex, push to the front
