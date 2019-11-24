c89b5e5097757f6ab1e9a97c45f8f8ea078d177a
Alejandro Chang <alejandrochang10@gmail.com>
2019-11-24 13:59:11 -0800

    solution for maxSubArray problem for both 0 and lowest from array

--- InterviewPrep/masteringCodingInt/Arrays/masSubArraySum.js
+++ InterviewPrep/masteringCodingInt/Arrays/masSubArraySum.js
@@ -4,16 +4,31 @@
 let allNegatives = (arr) => arr.every(num => num < 0);
 let allPositives = (arr) => arr.every(num => num > 0);
 
+// assuming lowest number should be 0
+var maxSubArray = function(nums) {
+  if (nums.length == 1) return nums[0];
+  if (allNegatives(nums)) return 0;
+  if (allPositives(nums)) return nums.reduce((sum, curr) => sum += curr);
+
+  let current = 0;
+  let max = 0;
+  
+  for (let i = 0; i < nums.length; i++) {
+      current = Math.max(current, nums[i] + current);
+      max = Math.max(current, max);
+  }
+  
+  return max;
+};
+
+// maxSubArray inclduing negative numbers -> lowest number has to come from array and not 0
 const maxSubArraySum = (nums) => {
-    // if (allNegatives(nums)) return 0;
-    // if (allPositives(nums)) return nums.reduce((sum, curr) => sum += curr);
     let current = 0;
-    let max = 0;
+    let max = Number.NEGATIVE_INFINITY;
     
     for (let i = 0; i < nums.length; i++) {
-      // console.log(nums[i])
         current = Math.max(nums[i], nums[i] + current);
-        max =current;
+        max = Math.max(current, max);
     }
     
     return max;
@@ -22,5 +37,6 @@ const maxSubArraySum = (nums) => {
 const arr1 = [-2, -1];
 
 console.log(maxSubArraySum(arr1)); // -1
+console.log(maxSubArraySum([-2,1,-3,4,-1,2,1,-5,4])); // 6
 // console.log(allNegatives(arr)); // false
 // console.log(allNegatives(arr1)); // false
