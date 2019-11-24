accdeaea67c5f002b3ad01d8acd2b9532684cb6d
Alejandro Chang <alejandrochang10@gmail.com>
2019-11-24 14:16:30 -0800

    moveZeroes solution

---
+++ InterviewPrep/masteringCodingInt/Arrays/moveZeroes.js
@@ -0,0 +1,25 @@
+
+
+// Input: [0,1,0,3,12]
+// Output: [1,3,12,0,0]
+
+const moveZeroes = (arr) => {
+  let k = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] !== 0) {
+      let temp = arr[i]; // number that is not 0
+      arr[i] = arr[k]; // set whatever is not a numebr to that variable
+      arr[k] = temp; // push the number to the front
+      k++;
+    }
+  }
+
+  return arr;
+}
+
+const arr = [0, 1, 0, 3, 12];
+console.log(moveZeroes(arr)); // [ 1, 3, 12, 0, 0 ]
+
+// iterate through array check to see if curr element is 0
+// if it  is 
