60b13c8c2bfb6858cb91d664b5d6e4c28f9f469c
Alejandro Chang <alejandrochang10@gmail.com>
2019-11-24 18:12:34 -0800

    clearer solution

--- InterviewPrep/masteringCodingInt/Strings/longestString.js
+++ InterviewPrep/masteringCodingInt/Strings/longestString.js
@@ -4,7 +4,8 @@
 
 const longestWord = (sen) => {
   let result = '';
-  for (let word of sen.replace(/[^\w]/g, ' ').split(' ')) {
+  let words = sen.replace(/[^\w]/g, ' ').split(' ');
+  for (let word of words) {
     if (result.length < word.length) {
       result = word;
     }
@@ -15,7 +16,7 @@ const longestWord = (sen) => {
 
 // Input: "fun&!! time"
 // Output: time
-// Input: "I love dogs"
+// Input: "I love dogs"cle
 // Output: love
 
 console.log(longestWord("fun&!! time")); // time
