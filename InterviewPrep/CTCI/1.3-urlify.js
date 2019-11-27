032097973cac666d1e5cc663c80073ef4cf739f4
Alejandro Chang <alejandrochang10@gmail.com>
2019-11-27 12:04:36 -0800

    urlify with trim method

--- InterviewPrep/CTCI/1.3-urlify.js
+++ InterviewPrep/CTCI/1.3-urlify.js
@@ -7,11 +7,20 @@
 
 
 const urlify = (str) => {
-
+  return str.trim().split(' ').join('%20');
 }
+
+// trim removes extra spaces on back and end
 
 console.log(urlify('hello world')); // 'hello%20world'
 console.log(urlify('that how it works !   ')); // 'that%20how%20it%20works%20!'
 
 
 // [pseudo]
+// iterate through string, if its a space append the %20
+// if the last is %20 and the current is a space, continue
+
+// Time: O(n)
+// Spacee: O(n)
+
+// go backwards and check if we have hit our first char
