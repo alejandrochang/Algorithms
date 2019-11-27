dde5509a58cbdb78039a5428830679ea8ab5bcbe
Alejandro Chang <alejandrochang10@gmail.com>
2019-11-27 12:23:31 -0800

    palindrome permutations cleaner solution

--- InterviewPrep/CTCI/1.4-palindromePermutations.js
+++ InterviewPrep/CTCI/1.4-palindromePermutations.js
@@ -2,10 +2,31 @@
 // permutation ex. ab -> ab, ba
 
 
-console.log(palindromPermutation("Tact Coa")); // "true"
+const palindromePermutation = (str) => {
+  let map = {};
+  for (let ch of str.toLowerCase()) {
+    if (ch === ' ') continue;
+    if (!map[ch]) {
+      map[ch] = true
+    } else {
+      delete map[ch];
+    }
+  }
+
+  return Object.keys(map).length <= 1;
+}
+
+console.log(palindromePermutation("Tact Coa")); // "true"
 // True (permutations: "taco cat", "atco cat")
 
-console.log(palindromPermutation("aab")); // "true
-console.log(palindromPermutation("carrace")); // "true
-console.log(palindromPermutation("Tact boa")); // "false"
-console.log(palindromPermutation("code")); // "false"
+console.log(palindromePermutation("aab")); // "true
+console.log(palindromePermutation("carrace")); // "true
+console.log(palindromePermutation("Tact boa")); // "false"
+console.log(palindromePermutation("code")); // "false"
+
+
+// [pseudo]
+// We can count palindrom permutations by keeping count of keys,
+// keys cannot be greater than 1
+// add to a map, remove from a map
+// if map.length is > 1, it is not a palindrome permutation
