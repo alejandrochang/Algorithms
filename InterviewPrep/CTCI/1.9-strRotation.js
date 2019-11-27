7de5f9a356847367a12343546cfb4e05c60b8123
Alejandro Chang <alejandrochang10@gmail.com>
2019-11-27 13:58:03 -0800

    substring solution

--- InterviewPrep/CTCI/1.9-strRotation.js
+++ InterviewPrep/CTCI/1.9-strRotation.js
@@ -4,8 +4,12 @@
 // write code to check if s2 is a rotation of s1 using only one call 
 // to isSubstring (ex. waterboottle is a rotation of erbottlewat)
 
+const stringRotation = (s1, s2) => {
+  let fullString = s2 + s2;
+  return fullString.includes(s1);
+}
 
-const s1 = 'waterboottle';
+const s1 = 'waterbottle';
 const s2 = 'erbottlewat';
 
 console.log(stringRotation(s1, s2)); // true
