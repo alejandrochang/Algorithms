7747d999b70524e578d2067328c576d87a0d4318
Alejandro Chang <alejandrochang10@gmail.com>
2019-11-28 13:35:34 -0800

    rotate matrix progress

--- InterviewPrep/CTCI/1.7-rotateMatrix.js
+++ InterviewPrep/CTCI/1.7-rotateMatrix.js
@@ -16,22 +16,41 @@ const F = [
   [7, 8, 9]
 ]
 
-function rotate(matrix) {
-  const n = matrix.length;
-  const x = Math.floor(n/ 2);
-  const y = n - 1;
+const rotate = (matrix) => {
+  const x = Math.floor(matrix.length / 2);
+  const y = matrix.length - 1;
+
   for (let i = 0; i < x; i++) {
-     for (let j = i; j < y - i; j++) {
-        k = matrix[i][j];
-        matrix[i][j] = matrix[y - j][i];
-        matrix[y - j][i] = matrix[y - i][y - j];
-        matrix[y - i][y - j] = matrix[j][y - i]
-        matrix[j][y - i] = k
-     }
+    
   }
 }
+
+// function rotate(matrix) {
+//   const n = matrix.length;
+//   const x = Math.floor(n/ 2);
+//   const y = n - 1;
+//   for (let i = 0; i < x; i++) {
+//      for (let j = i; j < y - i; j++) {
+//         k = matrix[i][j];
+//         matrix[i][j] = matrix[y - j][i];
+//         matrix[y - j][i] = matrix[y - i][y - j];
+//         matrix[y - i][y - j] = matrix[j][y - i]
+//         matrix[j][y - i] = k
+//      }
+//   }
+// }
 rotate(A);
 console.log(A);
+
+
+
+// Input: 
+// const A = [
+//   [1, 2, 3, 4],
+//   [5, 6, 7, 8],
+//   [9, 10, 11, 12],
+//   [13, 14, 15, 16]
+// ]
 
 // Solution:
 // [ [ 13, 9, 5, 1 ],
