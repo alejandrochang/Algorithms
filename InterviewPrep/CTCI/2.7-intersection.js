ed02f578b48f15b6d20fcc0a586b50fad339ad35
Alejandro Chang <alejandrochang10@gmail.com>
2019-12-03 16:05:32 -0800

    intersection solution

--- InterviewPrep/CTCI/2.7-intersection.js
+++ InterviewPrep/CTCI/2.7-intersection.js
@@ -21,11 +21,34 @@ class LinkedList {
   }
 }
 
-let ll = new LinkedList();
-ll.insertFirst('e');
-ll.insertFirst('d');
-ll.insertFirst('c');
-ll.insertFirst('b');
-ll.insertFirst('a');
+let l1 = new LinkedList();
+l1.insertFirst('e');
+l1.insertFirst('d');
+l1.insertFirst('c');
+l1.insertFirst('b');
+l1.insertFirst('a');
+// a -> b -> c -> d -> e
 
+let l2 = new LinkedList();
+l2.insertFirst(5);
+l2.insertFirst(7);
+l2.insertFirst('c');
+l2.insertFirst(1);
+l2.insertFirst(9);
+// 5 -> 7 -> c -> 1 -> 9
+
+const intersection = (l1, l2) => {
+  let node1 = l1.head;
+  let node2 = l2.head;
+
+  while (node1 && node2) {
+    if (node1.data === node2.data) return node1;
+    node1 = node1.next;
+    node2 = node2.next
+  }
+
+  return null;
+}
+
+console.log(intersection(l1, l2)); // returns Node: C
 
