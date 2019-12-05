e215f287eaccec6642bb3d028ee8a9ae484fbce1
Alejandro Chang <alejandrochang10@gmail.com>
2019-12-04 19:25:17 -0800

    solution to sort stack

--- InterviewPrep/CTCI/3.5-sortStack.js
+++ InterviewPrep/CTCI/3.5-sortStack.js
@@ -4,10 +4,47 @@
 // into another structure such as an array. The stack supports these operations:
 // push, pop, peek, isEmpty
 
-class sortStack {
+class Stack {
   constructor() {
     this.data = [];
   }
 
-  
+  push(data) {
+    this.data.push(data);
+  }
+
+  pop() {
+    return this.data.pop();
+  }
+
+  isEmpty() {
+    return this.data.length === 0;
+  }
+
+  peek() {
+    return this.data[this.data.length - 1];
+  }
 }
+
+const sortStack = (input) => {
+  let tempStack = new Stack();
+
+  while (!input.isEmpty()) {
+    let temp = input.pop();
+    while (!tempStack.isEmpty() && tempStack.peek() > temp) {
+      input.push(tempStack.pop());
+    }
+
+    tempStack.push(temp);
+  }
+
+  return tempStack;
+}
+
+let s1 = new Stack();
+s1.push(5);
+s1.push(75);
+s1.push(56);
+s1.push(115);
+s1.push(2);
+console.log(sortStack(s1));
