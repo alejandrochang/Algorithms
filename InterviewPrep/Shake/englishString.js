0b01a783ae31bb8ec09becdda84fcf7ec83ceda2
Alejandro Chang <alejandrochang10@gmail.com>
2019-12-05 07:45:59 -0800

    more problems related to stacks

---
+++ InterviewPrep/Shake/englishString.js
@@ -0,0 +1,19 @@
+// Had to write a method that would
+// input a number and output the english string representation of that number  
+
+const englishString = (int) => {
+
+}
+
+console.log(englishString());
+
+// 1 -> one
+// 2 -> two
+// 3 -> three
+// 4 -> four
+// 5 -> five
+// 6 -> six
+// 7 -> seven
+// 8 -> eight
+// 9 -> nine
+// 10 -> ten
