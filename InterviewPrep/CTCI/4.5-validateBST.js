f0b2e36ca6b6f7fbf3a263cc7218f08d0742bea4
Alejandro Chang <alejandrochang10@gmail.com>
2019-12-10 11:15:57 -0800

    check balanced solution for trees"

--- InterviewPrep/CTCI/4.5-validateBST.js
+++ InterviewPrep/CTCI/4.5-validateBST.js
@@ -1,6 +1,4 @@
-// Implement a function to check if BST is balanced. For the purposes of this question,
-// a balanced tree is defined to be a tree such that the heights of 
-// the two subtrees of any node  never differ by more than 1
+// Validate a Binary Search Tree
 
 
 class Node {
@@ -28,28 +26,6 @@ class Tree {
   constructor(){
     this.root = null;
   }
-
-  isBalanced() {
-    let node = this.root;
-    if (!node) return true;
-    
-    // calculate left and right side heights count
-    let leftCount = this.height(node.left);
-    let rightCount = this.height(node.right);
-
-    // Balanced Tree difference can be 1 or less
-    if (Math.abs(leftCount - rightCount) <= 1) {
-      return true;
-    }
-
-    return false;
-  }
-
-  height(node) {
-    if (!node) return 0;
-    // balanced means could be at most 1 off, get the longest height
-    return 1 + Math.max(this.height(node.left), this.height(node.right));
-  }
 }
 
 let node = new Node(10);
@@ -60,12 +36,6 @@ node.insert(12);
 node.insert(5);
 node.insert(18);
 
-// adding a diff of two returns false
-// node.insert(100);
-// node.insert(10000);
-
-console.log(tree.isBalanced()); // true
-
 //     10
 //    7   12
 //  5        18
@@ -74,9 +44,30 @@ console.log(tree.isBalanced()); // true
 // console.log(JSON.stringify(node, null, 4));
 // console.log(JSON.stringify(tree, null, 4));
 
-// console.log(checkBalanced(tree.root));
+const validateBST = (node, min = null, max = null) => {
+  if (min !== null && node.data < min) {
+    return false;
+  }
 
-// [pseudo]: how do we know its balanced
+  if (max !== null && node.data > max) {
+    return false;
+  }
+
+  if (node.left && !validate(node.left, min, node.data)) {
+    return false;
+  }
+
+  if (node.right && !validate(node.right, node.data, max)) {
+    return false;
+  }
+
+
+  return true;
+}
+
+console.log(validateBST(tree.root));
+
+// [pseudo]: how do  we knoe its balanced
 // Hold a min, max values for the function
 // Go to left and validate the data store the max, should be less than
 // Go to right and store the min, should be greter than that
