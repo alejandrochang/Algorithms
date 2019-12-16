015cd95bd85aa59a9ffea7601fac5a1f6f25fa50
Alejandro Chang <alejandrochang10@gmail.com>
2019-12-16 13:07:35 -0800

    progress on 3 pointer

--- InterviewPrep/practice.js
+++ InterviewPrep/practice.js
@@ -1,8 +1,23 @@
 
 
-const solution = (array) => {
+const unique = (array) => {
+  let j = 1;
+  let r = 1;
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] !== array[j] || array[r].includes(array[i])) {
+      array[i] = [array[i], array[j]];
+      j++;
+      i++;
+    } 
+    
+    // else {
 
+    // }
+  }
 }
 
 
-// console.log(solution()); // 
+let arr = [1, 4, 5, 2, 1, 5, 2];
+console.log(unique()); //
+
+// [1, 4, 5, 2]
