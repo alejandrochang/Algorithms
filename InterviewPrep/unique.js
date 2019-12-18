518de76ae7bfc343c3d5960862930cd01e637467
Alejandro Chang <alejandrochang10@gmail.com>
2019-12-17 22:22:31 -0800

    progress

--- InterviewPrep/unique.js
+++ InterviewPrep/unique.js
@@ -2,8 +2,16 @@
 
 
 const unique = (arr) => {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[ Math.abs(arr[i])] >= 0) {
+      // arr[ Math.abs(arr[i])] = -arr[ Math.abs(arr[i])]; 
+    } else {
+      console.log(arr[i] + " ");
+    }
+  }
 
+  return arr;
 }
 
-let arr = []
-console.log(unique(arr));
+let array = [1, 2, 3, 4, 5, 6, 3]
+console.log(unique(array));
