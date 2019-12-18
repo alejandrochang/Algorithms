8bdc33cdb2881b74c149a41e1ee3432d03393e13
Alejandro Chang <alejandrochang10@gmail.com>
2019-12-18 14:56:58 -0800

    testing possible solution

--- InterviewPrep/unique.js
+++ InterviewPrep/unique.js
@@ -4,10 +4,10 @@
 const unique = (arr) => {
   for (let i = 0; i < arr.length; i++) {
     if (arr[ Math.abs(arr[i])] >= 0) {
-      // arr[ Math.abs(arr[i])] = -arr[ Math.abs(arr[i])]; 
-    } else {
-      console.log(arr[i] + " ");
-    }
+          arr[ Math.abs(arr[i])] = -arr[ Math.abs(arr[i])]; 
+      } else {
+        console.log(Math.abs(arr[i]) + " "); 
+      }
   }
 
   return arr;
