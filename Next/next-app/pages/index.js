dca41d201873b3808e04f5e6811e01f05dd09c32
Alejandro Chang <alejandrochang10@gmail.com>
2020-01-15 23:05:57 -0800

    further progress on project setup

--- Next/next-app/pages/index.js
+++ Next/next-app/pages/index.js
@@ -7,36 +7,12 @@ import '../styles/style.scss'
 const Home = () => (
   <div className="example">
     <Head>
-      <title>Home</title>
-      <link rel="icon" href="/favicon.ico" />
+      {/* <title>Home</title>
+      <link rel="icon" href="/favicon.ico" /> */}
     </Head>
 
     <Nav />
 
-    <div className="middle">
-      <h1 className="title">Welcome to Learn Finance Education!</h1>
-      <p className="description">
-        To get started, edit <code>pages/index.js</code> and save to reload.
-      </p>
-
-      <div className="row">
-        <a href="https://nextjs.org/docs" className="card">
-          <h3>Documentation &rarr;</h3>
-          <p>Learn more about Next.js in the documentation.</p>
-        </a>
-        <a href="https://nextjs.org/learn" className="card">
-          <h3>Next.js Learn &rarr;</h3>
-          <p>Learn about Next.js by following an interactive tutorial!</p>
-        </a>
-        <a
-          href="https://github.com/zeit/next.js/tree/master/examples"
-          className="card"
-        >
-          <h3>Examples &rarr;</h3>
-          <p>Find other example boilerplates on the Next.js GitHub.</p>
-        </a>
-      </div>
-    </div>
   </div>
 )
 
