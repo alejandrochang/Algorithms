6a05b463a692e604a1c515e83fd5eb2f921b1aa7
Alejandro Chang <alejandrochang10@gmail.com>
2020-01-15 22:43:40 -0800

    enabling sass in project

--- Next/next-app/pages/index.js
+++ Next/next-app/pages/index.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import Head from 'next/head';
 import Nav from '../components/nav';
-import css from "../stylesheets/styles.scss"
 
-
-// import './style.css';
+import '../styles/style.scss'
 
 const Home = () => (
-  <div>
+  <div className="example">
     <Head>
       <title>Home</title>
       <link rel="icon" href="/favicon.ico" />
@@ -15,14 +13,14 @@ const Home = () => (
 
     <Nav />
 
-    <div className="hero">
+    <div className="middle">
       <h1 className="title">Welcome to Learn Finance Education!</h1>
-      {/* <p className="description">
+      <p className="description">
         To get started, edit <code>pages/index.js</code> and save to reload.
-      </p> */}
+      </p>
 
       <div className="row">
-        {/* <a href="https://nextjs.org/docs" className="card">
+        <a href="https://nextjs.org/docs" className="card">
           <h3>Documentation &rarr;</h3>
           <p>Learn more about Next.js in the documentation.</p>
         </a>
@@ -36,57 +34,9 @@ const Home = () => (
         >
           <h3>Examples &rarr;</h3>
           <p>Find other example boilerplates on the Next.js GitHub.</p>
-        </a> */}
+        </a>
       </div>
     </div>
-
-    {/* <style jsx>{`
-      .hero {
-        width: 100%;
-        color: #333;
-      }
-      .title {
-        margin: 0;
-        width: 100%;
-        padding-top: 80px;
-        line-height: 1.15;
-        font-size: 48px;
-      }
-      .title,
-      .description {
-        text-align: center;
-      }
-      .row {
-        max-width: 880px;
-        margin: 80px auto 40px;
-        display: flex;
-        flex-direction: row;
-        justify-content: space-around;
-      }
-      .card {
-        padding: 18px 18px 24px;
-        width: 220px;
-        text-align: left;
-        text-decoration: none;
-        color: #434343;
-        border: 1px solid #9b9b9b;
-      }
-      .card:hover {
-        border-color: #067df7;
-      }
-      .card h3 {
-        margin: 0;
-        color: #067df7;
-        font-size: 18px;
-      }
-      .card p {
-        margin: 0;
-        padding: 12px 0 0;
-        font-size: 13px;
-        color: #333;
-      }
-
-    `}</style> */}
   </div>
 )
 
