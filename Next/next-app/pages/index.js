b62d7183d64bb8131736660b182bb5ac7afa9b83
Alejandro Chang <alejandrochang10@gmail.com>
2020-01-16 19:47:45 -0800

    further configurations for scss

--- Next/next-app/pages/index.js
+++ Next/next-app/pages/index.js
@@ -2,17 +2,13 @@ import React from 'react';
 import Head from 'next/head';
 import Nav from '../components/nav';
 
+
 import '../styles/style.scss'
 
 const Home = () => (
   <div className="example">
-    <Head>
-      {/* <title>Home</title>
-      <link rel="icon" href="/favicon.ico" /> */}
-    </Head>
-
+    <Head />
     <Nav />
-
   </div>
 )
 
