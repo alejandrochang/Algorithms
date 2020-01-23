c4777037cba66b53dda8119090a2e776734b095a
Alejandro Chang <alejandrochang10@gmail.com>
2020-01-22 21:03:42 -0800

    initializing problem

---
+++ Practice/shortestPath.js
@@ -0,0 +1,13 @@
+// Find the shortest path of a matrix
+
+const matrix = [
+    [0, 1, 0, 1, 0],
+    [0, 0, 1, 0, 0],
+    [0, 0, 1, 1, 0],
+    [0, 0, 0, 0, 0],
+    [0, 1, 0, 1, 0]
+ ];
+
+ let start = [4,0];
+ let end = [1,1];
+
