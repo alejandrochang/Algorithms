b2efdcd9d5348ca17cfc34ea9f8bed6eaeaad029
Alejandro Chang <alejandrochang10@gmail.com>
2020-01-25 17:27:42 -0800

    working on solving zeroes problem

--- Practice/moveZeroes.js
+++ Practice/moveZeroes.js
@@ -1,6 +1,17 @@
-// Move all zeroes from left to right
-
+// Move all zeroes to beginning of array
 
 const moveZeroes = (arr) => {
-
+  let lastIdx = 
 }
+
+let array = [5, 2, 0, 0, 5, 0, 8, 56, 102];
+console.log(moveZeroes(array));
+
+
+// pseudo
+// when encountering a number that is zero swap with the last number that is not zero
+// keep index of number that you're switching
+
+
+
+// Move all zeroes to end of array
