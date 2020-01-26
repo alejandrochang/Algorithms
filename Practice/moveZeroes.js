a27dc62a55b80f8a8d9e49cf213729b49019f506
Alejandro Chang <alejandrochang10@gmail.com>
2020-01-26 09:32:40 -0800

    further solutions"

--- Practice/moveZeroes.js
+++ Practice/moveZeroes.js
@@ -1,28 +1,49 @@
 // Move all zeroes to beginning of array
 
 const moveZeroes = (arr) => {
-  let lastIdx = 0;
+  let idx = 0;
   for (let i = 0; i < arr.length; i++) {
     // if its zero add to idx
     if (arr[i] === 0) {
-      let temp = arr[lastIdx];
-      arr[lastIdx] = arr[i];
+      let temp = arr[idx];
+      arr[idx] = arr[i];
       arr[i] = temp;
-      lastIdx++;
+      idx++;
     }
   }
 
   return arr;
 }
 
-let array = [5, 2, 0, 0, 5, 0, 8, 56, 102];
-console.log(moveZeroes(array));
+// let array = [5, 2, 0, 0, 5, 0, 8, 56, 102];
+// console.log(moveZeroes(array)); // [ 0, 0, 0, 2, 5, 5, 8, 56, 102 ]
+
 
 
 // pseudo
 // when encountering a number that is zero swap with the last number that is not zero
 // keep index of number that you're switching
 
-
-
 // Move all zeroes to end of array
+
+const moveZeroes2 = (arr) => {
+  let idx = arr.length - 1; // 
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === 0) {
+      let temp = arr[idx]; // 102
+      arr[idx] = arr[i];
+      arr[i] = temp;
+      idx--;
+    }
+  }
+
+  return arr;
+}
+
+
+let array2 = [5, 2, 0, 0, 5, 0, 8, 56, 102];
+console.log(moveZeroes2(array2)); // 
+
+
+// if the current number is zero
+// swap it with the beginning number
