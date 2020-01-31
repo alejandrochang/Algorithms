ca8168342422756acd8d71da6d1f0a5de63d0f39
Alejandro Chang <alejandrochang10@gmail.com>
2020-01-31 09:45:09 -0800

    updating next config file dev vs prod

--- Next/learnfinedu/.next/static/runtime/webpack.js
+++ Next/learnfinedu/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "9ddbb12ddc89f2ddc79e";
+/******/ 	var hotCurrentHash = "73ed2b9e5dece5cd6f8d";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- Next/learnfinedu/next.config.js
+++ Next/learnfinedu/next.config.js
@@ -1,5 +1,14 @@
-// next.config.js
-const withCSS = require('@zeit/next-css')
-module.exports = withCSS({
-  /* config options here */
-})
+const { PHASE_DEVELOPMENT_SERVER } = require('next/constants');
+
+module.exports = (phase, { defaultConfig }) => {
+  console.log({ phase });
+  if (phase === PHASE_DEVELOPMENT_SERVER) {
+    return {
+      /* development only config options here */
+    }
+  }
+
+  return {
+    /* config options for all phases except development here */
+  }
+}
