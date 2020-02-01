1037c0d7df405788cbb837327c43f6d0a48c2d15
Alejandro Chang <alejandrochang10@gmail.com>
2020-01-31 20:58:21 -0800

    updatign files

--- Next/learnfinedu/.next/static/runtime/webpack.js
+++ Next/learnfinedu/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "73ed2b9e5dece5cd6f8d";
+/******/ 	var hotCurrentHash = "3c6b309d813a03b4bd85";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
