fb9c933e6547a2475cc29c525b82c056d5e9f9d3
Alejandro Chang <alejandrochang10@gmail.com>
2020-02-12 23:31:14 -0800

    add two numbers solution

--- Practice/addTwoNumbers.js
+++ Practice/addTwoNumbers.js
@@ -1,20 +1,29 @@
+var addTwoNumbers = function(l1, l2) {
+    let top = { val: 0, next: null }
+    let head = top; 
+    let carry = 0;
+    while(l1 || l2){ 
+        l1 = l1 || { val: 0, next: null }
+        l2 = l2 || { val: 0, next: null }
+        let sum = l1.val + l2.val + carry
+ 
+        if(sum > 9){ 
+            carry = 1; 
+            sum = sum - 10; 
+        } else {
+            carry = 0; 
+        }
+       
+         head.next = { val: sum, next: null }; 
+         head = head.next; 
 
-
-let l1 = [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1]
-let l2 = [5,6,4]
-
-let l3 = [2,4,3]
-let l4 = [5,6,4]
-
-const addTwoNumbers = (l1, l2) => {
-
+        l1 = l1.next; 
+        l2 = l2.next; 
+        console.log({ top })
+    }
+  
+    if(carry !== 0){ 
+        head.next = { val: carry , next: null }
+    }
+    return top.next; 
 };
-
-
-console.log(addTwoNumbers(l1, l2)); // [6,6,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1]
-console.log(addTwoNumbers(l3, l4)); // [7,0,8]
-
-
-
-// pseudo
-// 
