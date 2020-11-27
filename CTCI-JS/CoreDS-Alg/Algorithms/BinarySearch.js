97c4949ee44fa9df8935f73aa8cd0bd9bafab761
Alejandro Chang <alejandrochang10@gmail.com>
2020-11-27 11:18:51 -0800

    adding missed files

--- CTCI-JS/CoreDS-Alg/Algorithms/BinarySearch.js
+++ CTCI-JS/CoreDS-Alg/Algorithms/BinarySearch.js
@@ -6,16 +6,22 @@ function bSearch(arr, target) {
   }
 
   const mid = Math.floor(arr.length / 2);
+  console.log({ arr, mid, target })
+  // const midIndex = 
   if (arr[mid] === target) {
+    console.log({ mid })
     return mid;
   } else if (arr[mid] > target) {
+    console.log(arr, mid)
     return bSearch(arr.slice(0, mid), target);
   } else if (arr[mid] < target) {
     let result = bSearch(arr.slice(mid + 1), target);
+    console.log({ result })
     return result === null ? null : result + mid + 1;
   }
 }
 
-console.log(bSearch([1, 2, 3, 4, 5, 6, 7], 4)); // 3
-console.log(bSearch([1, 2, 3, 4, 5, 6, 7], 10)); // null
+console.log(bSearch([1, 3, 16, 22, 31, 33, 34], 31)); // 4
+// console.log(bSearch([1, 2, 3, 4, 5, 6, 7], 4)); // 3
+// console.log(bSearch([1, 2, 3, 4, 5, 6, 7], 10)); // null
 
---
+++ intgoog/Array/printSubarraysEqualZero.js
@@ -0,0 +1,11 @@
+
+
+function printSubarraysEqualToZero() {
+
+}
+
+
+
+console.log(printSubarraysEqualToZero([4,2,-3,-1,0,4]));
+// [3,1,0,4], // [0]
+
