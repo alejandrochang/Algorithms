3f4b92a58129c81f0745e8d40614fa4787051153
Alejandro Chang <alejandrochang10@gmail.com>
2022-01-31 13:48:10 -0800

    same tree sol

--- prep22/2.91maxAndMinValuesBST.js
+++ prep22/2.91maxAndMinValuesBST.js
@@ -46,3 +46,54 @@ function findMax(node) {
 
 // console.log("Max:", findMax(root)); // Node { 30 };
 
+
+let arr = [];
+
+// inOrder gets left side first, ends right side last
+
+function inorder(node) {
+  node.left && inorder(node.left);
+  arr.push(node.val);
+  node.right && inorder(node.right);
+}
+
+inorder(root);
+
+// console.log('min:', arr[0], 'max:', arr[arr.length - 1]); // 13, 30
+
+
+// function isSamTree(root1, root2) {
+//   return JSON.stringify(root1) === JSON.stringify(root2);
+// }
+
+
+
+const left2 = new Node(15, new Node(13), new Node(18));
+const right2 = new Node(25, null, new Node(30));
+const root2 = new Node(20, left2, right2);
+
+const left3 = new Node(15, new Node(13), new Node(22));
+const right3 = new Node(25, null, new Node(30));
+const root3 = new Node(20, left3, right3);
+
+
+// if root1 !== roor2 return false
+// isSameTree(left, left)
+// isSameTree(right, right)
+
+
+// if tree1 && tree2 reutnr true
+// if tree1 || tree2 reutnr false
+// check value + more
+
+function isSameTree(tree1, tree2) {
+  if (!tree1 && !tree2) return true;
+  if (!tree1 || !tree2) return false;
+
+  return (
+    tree1.val === tree2.val && isSameTree(tree1.left, tree2.left) && isSameTree(tree1.right, tree2.right)
+  )
+}
+
+console.log('same tree', isSameTree(root, root2)); // true
+console.log("same tree", isSameTree(root, root3)); // false
