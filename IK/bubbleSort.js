33f14e06bd0a32e8f0873d1e541fa37b2a13d485
Alejandro Chang <alejandrochang10@gmail.com>
2022-02-09 20:35:42 -0800

    moresorting

---
+++ IK/bubbleSort.js
@@ -0,0 +1,22 @@
+
+
+
+function bubbleSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length; j++) {
+      if (arr[i] < arr[j]) {
+        [arr[i], arr[j]] = [arr[j], arr[i]];
+      }
+    }
+  }
+
+  return arr;
+}
+
+
+const test = [100, 5, 2, 1, 23, 12, 1];
+// console.log({ test });
+console.log('res', bubbleSort(test)); //[1,1,2,5,12,23,100]
+
+// Time: worst, best, average  - O(n^2)
+// Space: O(1)
