02a22dd3ab2df15c241f68a3fc71f174135c3806
Alejandro Chang <alejandrochang10@gmail.com>
2022-02-12 17:37:28 -0800

    merge sort completion

--- IK/mergeSort.js
+++ IK/mergeSort.js
@@ -13,9 +13,27 @@
 
 
 function mergeSort(arr) {
+  if (arr.length <= 1) return arr;
 
+  const mid = Math.floor(arr.length / 2);
+  const left = mergeSort(arr.slice(0, mid));
+  const right = mergeSort(arr.slice(mid));
+
+  return merge(left, right);
 }
 
+function merge(left, right) {
+  let res = [];
+  while (left.length && right.length) {
+    if (left[0] < right[0]) {
+      res.push(left.shift())
+    } else {
+      res.push(right.shift())
+    }
+  }
+
+  return [...res, ...left, ...right];
+}
 
 console.log(mergeSort([12, 400, 105, 2, 27, 7, 45, 1, 10])); // [ 1, 2, 7, 10, 12, 27, 45, 105, 400 ]
 var c = [33, 103, 3, 726, 200, 984, 198, 764, 9];
