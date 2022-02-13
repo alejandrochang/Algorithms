a834722338ea7b56dfcbd662c456c28bc95cd84e
Alejandro Chang <alejandrochang10@gmail.com>
2022-02-12 17:35:27 -0800

    continues work

--- IK/mergeSort.js
+++ IK/mergeSort.js
@@ -17,7 +17,6 @@ function mergeSort(arr) {
 }
 
 
-
 console.log(mergeSort([12, 400, 105, 2, 27, 7, 45, 1, 10])); // [ 1, 2, 7, 10, 12, 27, 45, 105, 400 ]
 var c = [33, 103, 3, 726, 200, 984, 198, 764, 9];
-console.log(mergeSort(c));
+console.log(mergeSort(c)); // [3,9,33,103,198, 200, 726, 764,984]
