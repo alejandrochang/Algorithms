a6cb7b60bb17d9042d3ca5c872103780c4f37b1d
Alejandro Chang <alejandrochang10@gmail.com>
2022-02-12 17:29:41 -0800

    examples with sort

---
+++ IK/mergeSort.js
@@ -0,0 +1,23 @@
+
+
+
+
+/* 
+
+  Notes:
+  - Gets the midpoint - splits array into 2 - uses binary search
+
+
+
+*/
+
+
+function mergeSort(arr) {
+
+}
+
+
+
+console.log(mergeSort([12, 400, 105, 2, 27, 7, 45, 1, 10])); // [ 1, 2, 7, 10, 12, 27, 45, 105, 400 ]
+var c = [33, 103, 3, 726, 200, 984, 198, 764, 9];
+console.log(mergeSort(c));
