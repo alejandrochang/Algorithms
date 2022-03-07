6a8d051321cb9f7c042cc3310a0e32e67b7f6c6b
Alejandro Chang <alejandrochang10@gmail.com>
2022-03-07 10:33:41 -0800

    permutations

--- IK/permutations.js
+++ IK/permutations.js
@@ -2,20 +2,20 @@
 const permutator = (inputArr) => {
   let result = [];
 
-  const permute = (arr, m = []) => {
+  function permute(arr, temp) {
     if (arr.length === 0) {
-      result.push(m);
-    } else {
-      for (let i = 0; i < arr.length; i++) {
-        let curr = arr.slice();
-        let next = curr.splice(i, 1);
-        console.log({ curr, next, arr, m });
-        permute(curr, m.concat(next));
-      }
+      result.push(temp);
     }
-  };
 
-  permute(inputArr);
+    for (let i = 0; i < arr.length; i++) {
+      let curr = arr.splice(i, 1)[0];
+      permute(arr, temp.concat(curr));
+      // splice one more time with current
+      arr.splice(i, 0, curr);
+    }
+  }
+
+  permute(inputArr, []);
 
   return result;
 };
--- IK/targetSumSubsetsCount.js
+++ IK/targetSumSubsetsCount.js
@@ -28,26 +28,60 @@
 
 // let result = checkIfSumPossible([2,4,8], 6);
 // let result = check_if_sum([2, 4, 8], 6);
-let result = check_if_sum([10, 20], 0);
-console.log({ result }); // 1
+// let result = check_if_sum([10, 20], 0);
+// console.log({ result }); // 1
 
-function check_if_sum(arr, k) {
-  let count = 0;
-  const start = arr[0];
+// function check_if_sum(arr, k) {
+//   let count = 0;
+//   const start = arr[0];
 
-  function buildSum(array, sum, inputArr) {
-    if (!inputArr.length) return;
-    if (sum === k) {
-      console.log({ sum, k })
-      count++;
+//   function buildSum(array, sum, inputArr) {
+//     if (!inputArr.length) return;
+//     if (sum === k) {
+//       console.log({ sum, k })
+//       count++;
+//     }
+
+//     for (let i = 1; i < inputArr.length; i++) {
+//       const curr = inputArr.shift();
+//       buildSum([...array, curr], sum + curr, inputArr);
+//     }
+//   }
+
+//   buildSum([], start, arr);
+//   return count > 0;
+// }
+
+const arr = [2,4,8];
+const target = 6;
+
+function checkIfSumPoss(arr, k) {
+  function helper(arr, i, k, sum, size) {
+    if (sum === k && size >= 1) {
+      return true;
     }
 
-    for (let i = 1; i < inputArr.length; i++) {
-      const curr = inputArr.shift();
-      buildSum([...array, curr], sum + curr, inputArr);
+    if (i === arr.length) {
+      return false;
     }
+
+    if (helper(arr, i + 1, k, sum, size) === true) {
+      return true;
+    }
+
+    return helper(arr, i + 1, k, sum + arr[i], size + 1);
   }
 
-  buildSum([], start, arr);
-  return count > 0;
+  return helper(arr, 0, k, 0, 0);
 }
+
+let solution = checkIfSumPoss(arr, target);
+console.log({ solution });
+// brute force - generate and store subsets
+// subset sum === k?
+
+// Time: O(2^n)
+// Space: O(n) + O(n) + O(1)
+
+// Optimal:
+// arr, index, k, sum, size of array
