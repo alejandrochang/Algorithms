8cb30bbe51e3ff70da5a88c5bdfa6d3b00ef5213
Alejandro Chang <alejandrochang10@gmail.com>
2022-03-17 11:30:06 -0700

    new que

---
+++ IK/strDecoding.js
@@ -0,0 +1,19 @@
+
+
+
+
+
+
+
+
+// Input : str[] = "1[b]"
+// Output : b
+
+// Input : str[] = "2[ab]"
+// Output : abab
+
+// Input : str[] = "2[a2[b]]"
+// Output : abbabb
+
+// Input : str[] = "3[b2[ca]]"
+// Output : bcacabcacabcaca
