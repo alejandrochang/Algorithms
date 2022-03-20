34350b8f5378e901130a155c421c4645417aff76
Alejandro Chang <alejandrochang10@gmail.com>
2022-03-20 08:58:35 -0700

    more problems and sols

--- IK/strDecoding.js
+++ IK/strDecoding.js
@@ -1,19 +1,17 @@
 
 
 
+function strDecoding(str) {
+  
+}
 
 
+const test = "1[b]"; // Output : b
+const test1 = "2[ab]"; // Output : abab
+const test2 = "2[a2[b]]"; // Output : abbabb
+const test3 = "3[b2[ca]]"; // Output : bcacabcacabcaca
 
-
-
-// Input : str[] = "1[b]"
-// Output : b
-
-// Input : str[] = "2[ab]"
-// Output : abab
-
-// Input : str[] = "2[a2[b]]"
-// Output : abbabb
-
-// Input : str[] = "3[b2[ca]]"
-// Output : bcacabcacabcaca
+console.log("0", strDecoding(test));
+console.log("1", strDecoding(test1));
+console.log("2", strDecoding(test2));
+console.log("3", strDecoding(test3));
