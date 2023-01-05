aab2b185f0bc24046d5afe2d19657e9b05422498
Alejandro Chang <alejandrochang10@gmail.com>
2023-01-04 18:45:28 -0800

    two sum space optimized

--- 50common/twoSum.js
+++ 50common/twoSum.js
@@ -10,6 +10,7 @@
 // Input: nums = [3,3], target = 6
 // Output: [0,1]
 
+// brute force time: O(n^2), space: O(1)
 function twoSum(nums, target) {
   for (let i = 0; i < nums.length; i++) {
     for (let j = 0; j < nums.length - 1; j++) {
@@ -26,4 +27,24 @@ const nums = [2,7,11,15];
 const target = 9;
 // Output: [0,1]
 
-console.log('Result:', twoSum(nums, target))
+
+function twoSum2(nums, target) {
+  const map = {};
+  let chosenIdx = null;
+  let chosenPosition = null;
+
+  for (let i = 0; i < nums.length; i++) {
+    if (map[nums[i]]) {
+      chosenIdx = i;
+      chosenPosition = target - nums[i];
+    }
+    let diff = target - nums[i];
+    map[diff] = true;
+  }
+
+  console.log({ map })
+
+  return [nums.indexOf(chosenPosition), chosenIdx];
+}
+
+console.log('Result:', twoSum2(nums, target))
