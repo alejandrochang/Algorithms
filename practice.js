2ef4d352e42b92124df798f7366551ff5c70a105
Alejandro Chang <alejandrochang10@gmail.com>
2023-01-25 08:39:06 -0800

    teaching js

---
+++ practice.js
@@ -0,0 +1,24 @@
+
+// // const, let, var
+// const dog = "macy";
+// let dog2 = 'buddy';
+
+
+// // function hello() {
+// //   let dog = "macy";
+// //   // let dog = "macy";
+// //   // console.log({ dog });
+// //   console.log('hello', { dog });
+// // }
+
+// // console.log(dog); // 
+// // hello();
+
+
+
+
+
+
+
+
+
