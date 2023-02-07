95e432d53945d97d199ba1e7997bb98cd830173e
Alejandro Chang <alejandrochang10@gmail.com>
2023-02-07 14:23:16 -0800

    example test

--- Practice/rightSide.js
+++ Practice/rightSide.js
@@ -8,3 +8,17 @@
 //    5     4
 
 
+
+class Node {
+  constructor(val, left=null, right=null) {
+    this.val = val;
+    this.left = left;
+    this.right = right;
+  }
+}
+
+const root = new Node(1, new Node(2, null, new Node(5)), new Node(3, null, new Node(4)));
+// [1,3,4]
+
+
+// Run BFS - get last node of each level
