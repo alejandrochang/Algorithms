a7bda9b19fd40cec46b9d909e155337b15e8db02
Alejandro Chang <alejandrochang10@gmail.com>
2023-02-07 14:53:36 -0800

    right side solution bfs

--- Practice/rightSide.js
+++ Practice/rightSide.js
@@ -19,6 +19,34 @@ class Node {
 
 const root = new Node(1, new Node(2, null, new Node(5)), new Node(3, null, new Node(4)));
 // [1,3,4]
-
+const root2 = new Node(1, new Node(2, null, new Node(5, new Node(1))), new Node(3, null, new Node(4)));
+// [1,3,4, 1]
 
 // Run BFS - get last node of each level
+
+function btRigthSide(root) {
+  if (!root) return [];
+  const q = [root];
+  const result = [];
+
+  while (q.length > 0) {
+    let length = q.length;
+
+    for (let i = 0; i < length; i++) {
+      let node = q.shift();
+      if (i === length - 1) {
+        result.push(node.val)
+      }
+      if (node.left) q.push(node.left);
+      if (node.right) q.push(node.right);
+    }
+  }
+
+  return result;
+}
+
+
+let result = btRigthSide(root);
+
+let result2 = btRigthSide(root2);
+console.log({ result, result2 });
