1521353befad210d6768c7bb06a0d46fd1d04275
Alejandro Chang <alejandrochang10@gmail.com>
2023-02-13 23:03:46 -0800

    functions examples

--- Go/cards/main.go
+++ Go/cards/main.go
@@ -9,14 +9,21 @@ func main() {
 	// var card string = "Ace of spades"
 	card := "Ace of Spades"
 	card = "Five of Diamonds"
-	deckSize = 50
+	deckSize := 50
+	divisibleNum := 50
 	fmt.Println(card)
-	fmt.Println(deckSize)
+
+	var result int = addNumbers(deckSize, deckSize)
+	fmt.Println(result)
+
+	var additionRes, multipRes, diviRes = calculateOperations(deckSize, divisibleNum)
+	fmt.Println("addition:", additionRes, "multiplication", multipRes, "division", diviRes)
 }
 
-// func addNumbers() {
-// 	var num1 int = 10
-// 	var num2 int = 100
-// 	var result int = num1 + num2
-// 	return result
-// }
+func addNumbers(num1 int, num2 int) int {
+	return num1 + num2
+}
+
+func calculateOperations(num1, num2 int) (int, int, int) {
+	return num1 + num2, num1 * num2, num1 / num2
+}
