4d4cf71baa14be96d9839255edc935d99017cb37
Alejandro Chang <alejandrochang10@gmail.com>
2023-02-25 19:20:44 -0800

    feat: post request sample and test

--- weekmanage/server/main.go
+++ weekmanage/server/main.go
@@ -2,18 +2,43 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/gofiber/fiber/v2"
 )
+
+type Todo struct {
+	ID    int    `json:"id"`
+	Title string `json:"title"`
+	Done  bool   `json:"done"`
+	Body  string `json:"body"`
+}
 
 func main() {
 	fmt.Print("Hello World")
 
 	app := fiber.New()
 
-	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello, World!")
+	todos := []Todo{}
+
+	app.Get("/healthcheck", func(c *fiber.Ctx) error {
+		return c.SendString("Ok!")
 	})
 
-	app.Listen(":3000")
+	app.Post("/api", func(c *fiber.Ctx) error {
+		todo := &Todo{}
+
+		if err := c.BodyParser(todo); err != nil {
+			return err
+		}
+
+		todo.ID = len(todos) + 1
+
+		todos = append(todos, *todo)
+
+		// return list of todos
+		return c.JSON(todos)
+	})
+
+	log.Fatal(app.Listen(":4000"))
 }
