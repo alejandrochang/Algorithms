2372f19c005c29fd7b2d74398f041726f8687738
Alejandro Chang <alejandrochang10@gmail.com>
2023-02-27 20:36:29 -0800

    alg: set up algorithm dpotd

---
+++ DailyProblems/facebookquestions/encodeMessageChar.js
@@ -0,0 +1,23 @@
+// This problem was asked by Facebook.
+// Given the mapping a = 1, b = 2, ... z = 26, and an encoded message, count the number of ways it can be decoded.
+// For example, the message '111' would give 3, since it could be decoded as 'aaa', 'ka', and 'ak'.
+// You can assume that the messages are decodable. For example, '001' is not allowed.
+
+
+function encodeMessageChar(str) {
+  // if (!str) return 0;
+
+  // let dp = [];
+
+  // dp[str.length] = 1;
+
+  // string length 3 
+  // for (let i = string.length - 1; i >= 1; i--) {
+  //   dp[i] = dp[i + 1];
+  //   if (i < str.length - 1 && str)
+  // }
+}
+
+
+// Input: String
+// Output: Integer - Count of decodings 
