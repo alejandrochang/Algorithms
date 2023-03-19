cf82f0d5914840237d642cf46238114da5b9a22a
Alejandro Chang <alejandrochang10@gmail.com>
2023-03-18 19:20:48 -0700

    alg: setup string rotation

---
+++ ctci-nnew/1.9stringRotation.js
@@ -0,0 +1,13 @@
+// String Rotation:Assumeyou have a method isSubstring
+// which checks if one word is a substring of another. Given two strings, sl and s2,
+// write code to check if s2 is a rotation of sl using only one call to isSubstring (e.g.,"waterbottle" is a rotation of"erbottlewat").
+
+
+function stringRotation(str) {
+
+}
+
+
+/* 
+  
+*/
