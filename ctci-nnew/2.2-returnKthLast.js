11e124b57b73b57986f3c2f0e23a315b8006294d
Alejandro Chang <alejandrochang10@gmail.com>
2023-03-21 18:59:30 -0700

    alg: kth to last solutionN

--- ctci-nnew/2.2-returnKthLast.js
+++ ctci-nnew/2.2-returnKthLast.js
@@ -22,8 +22,39 @@ ll.insertFirst(4);
 ll.insertFirst(8);
 ll.insertFirst(12);
 ll.insertFirst(24);
+ll.insertFirst(8);
+ll.insertFirst(10);
 
 
-function kthLast(list) {
-  
+function kthLast(list, k) {
+  if (!list) return null;
+  if (!list.head.next) return list;
+  let slow = list.head;
+  let fast = list.head;
+
+  while (k > 0) {
+    fast = fast.next;
+    k--;
+  }
+
+  while (fast.next) {
+    slow = slow.next;
+    fast = fast.next;
+  }
+
+  return slow.data;
 }
+
+const result = kthLast(ll, 2);
+console.log(JSON.stringify(ll, null, 4));
+console.log({ result });
+
+// list: 4 -> 8 -> 12 -> 24 -> 8 -> 10
+// k = 2
+
+/*
+  Approaches:
+  1. Two pointers to nodes (fast and slow node) -> coutn difference from slow and fast to equal kth -end
+  Time: O(n) Space: O(1)
+
+*/
