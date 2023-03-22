87a0865fb8e237e349539214c22c866996d7a57f
Alejandro Chang <alejandrochang10@gmail.com>
2023-03-21 18:45:30 -0700

    alg: solution for remove dups linked list

--- ctci-nnew/2.1-removedDups.js
+++ ctci-nnew/2.1-removedDups.js
@@ -29,19 +29,33 @@ ll.insertFirst(6);
 ll.insertFirst(10);
 ll.insertFirst(15);
 
-console.log(JSON.stringify(ll, null, 4));
+// console.log(JSON.stringify(ll, null, 4));
 
 
 function removeDups(list) {
+  let node = list.head;
 
+  while (node.next) {
+    if (node.data === node.next.data) {
+      node.next = node.next.next;
+    } else {
+      node = node.next;
+    }
+  }
+
+  return list; // JSON.stringify(list, null, 4);
 }
+
+
+const result = removeDups(ll);
+console.log(JSON.stringify(result, null, 4)); // 
 
 
 // I: LinkedList
 // O: LinkedList (with no duplicates)
 
-
 /*
   Approaches:
-  1. Iterate through the nodes. While there
+  1. Iterate through the nodes. While next node -> if value is equal to curr nnode (keep going to next node)
+  Time: O(n), Space: O(1)
 */
