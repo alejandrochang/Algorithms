b39240660b650b6ab8fb15a9e9096024e9364bcb
Alejandro Chang <alejandrochang10@gmail.com>
2023-03-23 00:56:51 -0700

    alg: solution sum list ll

--- ctci-nnew/2.5-sumLists.js
+++ ctci-nnew/2.5-sumLists.js
@@ -26,20 +26,44 @@ const ll = new LinkedList();
 ll.insertFirst(6);
 ll.insertFirst(1);
 ll.insertFirst(7);
-// 7 -> 1 -> 6
+// 7 -> 1 -> 6 = 617
 
 const ll2 = new LinkedList();
 ll2.insertFirst(2);
 ll2.insertFirst(9);
 ll2.insertFirst(5);
-//5 -> 9 -> 2
+//5 -> 9 -> 2 = 295
+
+// 295 + 615 = 912
+// Return LL - 9 -> 1 -> 2
+
+const generateSum = (node) => {
+  let res = [];
+  while (node.next) {
+    res.push(String(node.data));
+    node = node.next;
+  }
+
+  if (node) res.push(String(node.data));
+
+  return Number(res.reverse().join(''))
+}
 
 
 function sumList(l1, l2) {
+  let l1Sum = generateSum(l1); //  
+  let l2Sum = generateSum(l2); //
+  const resultList = new LinkedList();
+  const sumArr = String(l1Sum + l2Sum).split('').reverse();
+  for (let s of sumArr) resultList.insertFirst(Number(s))
 
+  return resultList;
 }
 
+let result = sumList(ll.head, ll2.head);
+console.log(JSON.stringify(result, null, 4)); // 912
 /*
-  
 
+  1. Approach: iterate through linkedList - push up 
+  // ['7', '1', '6'] - reverse(). join() Number -> 617
 */
