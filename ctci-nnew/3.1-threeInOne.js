7b6a7d400c7c25738387d7a9333a77a5e03d7f37
Alejandro Chang <alejandrochang10@gmail.com>
2023-03-31 18:42:46 -0700

    alg: stack setup

--- ctci-nnew/3.1-threeInOne.js
+++ ctci-nnew/3.1-threeInOne.js
@@ -11,3 +11,44 @@ clear(): Removes all the items from the Stack.
 
 */
 
+// Single Array to implement 3 stacks
+// LIFO (LAST IN FIRST OUT)
+
+class Stack {
+  constructor() {
+    this.data = [];
+  }
+
+  push(el) {
+    this.data.push(el)
+  }
+
+  pop() {
+    console.log(this.data);
+    return this.data.shift()
+  }
+
+  peek() {
+    return this.data[0];
+  }
+
+  isEmpty() {
+    return this.data.length === 0;
+  }
+
+  clear() {
+    this.data = [];
+  }
+}
+
+const s = new Stack();
+s.push(5);
+s.push(10);
+s.push(15);
+
+// [5, 10, 15]
+
+console.log(s.peek()); // 5
+console.log(s.isEmpty()); // false
+console.log(s.pop()); // 5
+console.log(s.clear()); // undefined
