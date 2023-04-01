717f6998a81bdc948589388d287fca7832ad59e5
Alejandro Chang <alejandrochang10@gmail.com>
2023-03-31 18:36:40 -0700

    alg: adding methods for stack in text

---
+++ ctci-nnew/3.1-threeInOne.js
@@ -0,0 +1,13 @@
+// Three in One: Describe how you could use a single array to implement three stacks.
+
+// Methods in stack:
+/* 
+
+push(): Adds a single or multiple items to the top of the Stack.
+pop(): Removes and Returns the top item of the Stack.
+peek(): Returns the top item of the Stack.
+isEmpty(): Returns True if Stack is empty, False otherwise.
+clear(): Removes all the items from the Stack.
+
+*/
+
