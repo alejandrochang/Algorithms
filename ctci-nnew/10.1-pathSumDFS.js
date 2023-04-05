3327ce5cb6cba2d87fe79e82c818fa10aa668eb9
Alejandro Chang <alejandrochang10@gmail.com>
2023-04-04 18:54:54 -0700

    alg: full root to leaf solution

--- ctci-nnew/10.1-pathSumDFS.js
+++ ctci-nnew/10.1-pathSumDFS.js
@@ -1,3 +1,7 @@
+// Given the root of a binary tree and an integer targetSum,
+// return true if the tree has a root-to-leaf path such that adding up
+// all the values along the path equals targetSum.
+
 // Input: root = [5,4,8,11,null,13,4,7,2,null,null,null,1], targetSum = 22
 // Output: true
 // Explanation: The root-to-leaf path with the target sum is shown.
@@ -13,10 +17,37 @@
 target: 22
 */
 
+const getSum = (node) =>{
+  let sum = 0;
+  function rootToLeaf(node) {
+    if (!node) return 0;
+
+    rootToLeaf(node.left);
+    rootToLeaf(node.right);
+    sum += node.data;
+  }
+
+  rootToLeaf(node);
+
+  return sum;
+}
 
 const hasPathSum = (root, targetSum) => {
-
+  if (!root) return 0;
+  const left = getSum(root.left) + root.data;
+  const right = getSum(root.right) + root.data;
+  return (left === targetSum || right === targetSum);
 }
+
+
+
+// Has Path to Sum solution for full root -> leaf === target
+// const hasPathSumToLeaf = (root, targetSum) => {
+//   if (!root) return 0;
+//   const left = getSum(root.left) + root.data;
+//   const right = getSum(root.right) + root.data;
+//   return (left === targetSum || right === targetSum);
+// }
 
 
 class Node {
@@ -29,7 +60,7 @@ class Node {
 
 const root = new Node(5, new Node(4, new Node(11, new Node(7), new Node(2))), new Node(8, new Node(13), new Node(4, null, new Node(1))));
 
-console.log(JSON.stringify(root, null, 4));
+// console.log(JSON.stringify(root, null, 4));
 
 let result = hasPathSum(root, 22);
 console.log({ result }); // true
