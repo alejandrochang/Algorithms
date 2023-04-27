7f41ddbd963ab3ec63ad7966cdc87b9051712ea6
Alejandro Chang <alejandrochang10@gmail.com>
2023-04-27 16:33:47 -0700

    alg: example tuple

--- ctci-nnew/11.2-websiteUser.js
+++ ctci-nnew/11.2-websiteUser.js
@@ -1,4 +1,5 @@
-// You are given a list of (website, user) pairs that represent users visiting websites. Come up with a program that identifies the top k pairs of websites with the greatest similarity.
+// You are given a list of (website, user) pairs that represent users visiting websites.
+//  Come up with a program that identifies the top k pairs of websites with the greatest similarity.
 
 // For example, suppose k = 1, and the list of tuples is:
 
@@ -7,6 +8,15 @@
 //  ('c', 1), ('c', 2), ('c', 3), ('c', 4), ('c', 5)
 //  ('d', 4), ('d', 5), ('d', 6), ('d', 7),
 //  ('e', 1), ('e', 3), ('e': 5), ('e', 6)]
-// Then a reasonable similarity metric would most likely conclude that a and e are the most similar, so your program should return [('a', 'e')].
+// Then a reasonable similarity metric would most likely conclude that a and e are the most similar,
+// so your program should return [('a', 'e')].
 
 
+const tuples = [['a', 1], ['a', 3], ['a', 5],
+ ['b', 2], ['b', 6],
+ ['c', 1], ['c', 2], ['c', 3], ['c', 4], ['c', 5],
+ ['d', 4], ['d', 5], ['d', 6], ['d', 7],
+ ['e', 1], ['e', 3], ['e', 5], ['e', 6]];
+
+
+ 
