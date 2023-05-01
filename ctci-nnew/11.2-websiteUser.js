70c1333b59b6164fda9ebb61b9e8dd86a2a1cc86
Alejandro Chang <alejandrochang10@gmail.com>
2023-04-30 18:18:47 -0700

    alg: overall logic structure

--- ctci-nnew/11.2-websiteUser.js
+++ ctci-nnew/11.2-websiteUser.js
@@ -24,8 +24,25 @@ const tuples = [['a', 1], ['a', 3], ['a', 5],
 //  Keep track of similarities + differences
 
 function topSimilarities(list) {
-  const mostSimilar = [];
+  let charMap = {};
+  for (let i=0; i < list.length; i++) {
+    const [ch, count] = list[i];
+    if (charMap[ch]) {
+      charMap[ch].list.push(count)
+    } else {
+      charMap[ch] = { list: [count]};
+    }
+  }
 
+  const resultDiff = [];
+
+  // for (let key in charMap) { // key: a
+  //   const users = charMap[key]; // 1,3,5]
+
+
+  // }
+
+  console.log(charMap);
 
 }
 
@@ -34,17 +51,18 @@ function topSimilarities(list) {
 /*
 
 
-'b': { a: { sim: [], diff: [2, 6] }, c: { sim: 3, diff: 2 }}
+'b': { a: { sim: [], diff: [2, 6] }, c: { sim: 3, diff: 2 }
 
+
+ 1. Iterate and construct arr based off of chars - [{ ch: a, list: [1,3,5]}, { ch: b, list: [2,6]}]
+ 2. Iterate off of char, pull list - if list.includes(curr[1]) -> res [sim, diff] -> const diff = sim - diff > next override (save keys)
+
+{ '1': { similarities: ['a', 'b', 'c'], differences: []}}
 
 {
  'a': { b: [0, 3], c: [3, 2], e: [3, 1]
  'b': { a: [0, 2], c: [0, 5], d: [1, 3]
  }
-
-{ '1': { similarities: ['a', 'b', 'c'], differences: []}}
-
-
 */
 
 
