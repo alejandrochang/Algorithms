17973815cb6c653474df3300e57f4e9f927548af
Alejandro Chang <alejandrochang10@gmail.com>
2023-05-01 19:57:17 -0700

    alg: sorted solution

--- ctci-nnew/11-4.secondLargesNodeBST.js
+++ ctci-nnew/11-4.secondLargesNodeBST.js
@@ -15,4 +15,32 @@ class Node {
 }
 
 const root = new Node(8, new Node(3, new Node(1), new Node(6, new Node(4), new Node(7))), new Node(10, null, new Node(14, new Node(13))));
-console.log('tre', JSON.stringify(root, null, 4));
+// console.log('tre', JSON.stringify(root, null, 4));
+
+// Solution: 13
+// Time: O(n)
+// Space: O(n)
+
+// Inorder traversal -> return second to last node of arr
+
+
+function secondLargest(root) {
+  if (!root) return;
+  let sortedList = [];
+
+  function inorderTraverse(node) {
+    if (!node) return;
+
+    if (node.left) inorderTraverse(node.left);
+    sortedList.push(node.data);
+    if (node.right) inorderTraverse(node.right);
+  }
+
+  inorderTraverse(root);
+
+  return sortedList[sortedList.length - 2];
+}
+
+
+const result = secondLargest(root);
+console.log({ result });
