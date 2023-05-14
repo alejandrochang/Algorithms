eac4aa6b21073b089b11d2d32bde7fdb8b2a0437
Alejandro Chang <alejandrochang10@gmail.com>
2023-05-14 16:14:02 -0700

    alg: level sum with + 1

--- ctci-nnew/11-5.maxLevelSum.js
+++ ctci-nnew/11-5.maxLevelSum.js
@@ -21,7 +21,7 @@ class Node {
 
 const root = new Node(1, new Node(7, new Node(7)), new Node(0, new Node(-8)));
 
-function maxLevelSum(root) {
+function maxLevelSum2(root) {
   if (!root) return 0;
   const q = [root];
   let result = 0;
@@ -52,5 +52,55 @@ function maxLevelSum(root) {
   return {result, maxSum };
 }
 
-const result = maxLevelSum(root);
+// const result = maxLevelSum(root);
+// console.log({ result });
+
+
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxLevelSum = function(root) {
+    if (!root) return null;
+
+    let result = 0;
+    let maxSum = -Infinity;
+    let levels = 0;
+    const q = [root];
+    while (q.length > 0) {
+        const length = q.length;
+        let sum = 0;
+        for (let i = 0; i < length; i++) {
+            const curr = q.shift();
+            sum += curr.data;
+            if (curr.left) {
+                q.push(curr.left);
+            }
+            if (curr.right) {
+                q.push(curr.right);
+            }
+
+            if (sum > maxSum) {
+                maxSum = sum;
+                result = levels;
+            }
+        }
+
+        levels += 1;
+        sum = 0;
+    }
+
+    return result;
+};
+
+const result = maxLevelSum2(root);
 console.log({ result });
