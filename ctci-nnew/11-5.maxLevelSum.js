ae8e63b1690529b4237bf82b88361300d45f74f4
Alejandro Chang <alejandrochang10@gmail.com>
2023-05-14 15:32:07 -0700

    alg: max level sum

--- ctci-nnew/11-5.maxLevelSum.js
+++ ctci-nnew/11-5.maxLevelSum.js
@@ -23,8 +23,33 @@ const root = new Node(1, new Node(7, new Node(7)), new Node(0, new Node(-8)));
 
 function maxLevelSum(root) {
   if (!root) return 0;
+  const q = [root];
+  let result = 0;
+  let maxSum = -Infinity;
+  let level = 0;
 
-  
+  while (q.length > 0) {
+    const length = q.length;
+    let sum = 0;
+    for (let i = 0; i < length; i++) {
+      const node = q.shift();
+      sum += node.data;
+
+      if (node.left) q.push(node.left);
+      if (node.right) q.push(node.right);
+
+      if (sum > maxSum) {
+        maxSum = sum;
+        result = level;
+      }
+    }
+
+    level += 1;
+    sum = 0;
+  }
+
+
+  return {result, maxSum };
 }
 
 const result = maxLevelSum(root);
