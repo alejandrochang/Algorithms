c83e51c43a8e9eeb1e34f3f8c3082e111f228000
Alejandro Chang <alejandrochang10@gmail.com>
2023-06-09 18:24:30 -0700

    js: more examples

--- js-trivia.js
+++ js-trivia.js
@@ -6,3 +6,21 @@ Array.prototype.isEmpty = function() {
 const arr = [1, 2];
 console.log('returns false as its not empty', arr.isEmpty());
 
+const call = {
+  caller: "mom", 
+  says: function() {
+    console.log(`Hey, ${this.caller} just called.`);
+    // this is the call object
+  }
+};
+// call.says(); // Hey mom just called
+const call2 = {
+  caller: "mom", 
+  says: () => {
+    console.log(`Hey, ${this.caller} just called.`);
+  }
+};
+
+// call2.says(); // undefined - es6 arrow function will reference the outside global object which doesn't know about this
+
+
