772b8041c50e60d1cc65baf3452983043564cb6d
Alejandro Chang <alejandrochang10@gmail.com>
2023-06-11 17:30:34 -0700

    alg: shorten general struct

--- DailyProblems/shortenURL.js
+++ DailyProblems/shortenURL.js
@@ -9,3 +9,34 @@
 //  If no such shortened string exists, return null.
 // Hint: What if we enter the same URL twice?
 
+class URLShortener {
+  constructor() {
+    this.keys = new Map();
+  }
+
+  generateURL(url) {
+
+  }
+
+  shorten(url) {
+    // shorten(url), which shortens the url into a six-character alphanumeric string, such as zLg6wl.
+    const shortenedUrl = this.generateURL(url);
+    this.keys.set(shortenedUrl, url);
+  }
+
+  restore(short) {
+    // restore(short), which expands the shortened string into the original url.
+    //  If no such shortened string exists, return null.
+    const url = this.keys.get(short);
+    if (!url) return null;
+
+    return url;
+  }
+}
+
+// Hint: What if we enter the same URL twice?
+// Return the same url if it already exists in map
+
+
+// Solution:
+// { url: 6char }
