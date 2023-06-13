e0a409dde4e4901f12891a61b261febe8070ff7c
Alejandro Chang <alejandrochang10@gmail.com>
2023-06-12 20:30:43 -0700

    alg: solution to evalArithmetic

--- DailyProblems/evalArtihmetic.js
+++ DailyProblems/evalArtihmetic.js
@@ -12,11 +12,39 @@
 // 3  2  4  5
 // You should return 45, as it is (3 + 2) * (4 + 5).
 
-
-function eval(root) {
-
+class Node {
+  constructor(val, left=null, right=null) {
+    this.val = val;
+    this.left = left;
+    this.right = right;
+  }
 }
 
+function evaluation(root) {
+  if (!root) return 0;
+
+  if (!root.left && !root.right) {
+    return root.val;
+  }
+
+  const leftEval = evaluation(root.left);
+  const rightEval = evaluation(root.right);
+
+  if (root.val === '+') {
+    return leftEval + rightEval;
+  } else if (root.val === '-') {
+    return leftEval - rightEval;
+  } else if (root.val === '*') {
+    return leftEval * rightEval;
+  } else if (root.val === '/') {
+    return leftEval / rightEval;
+  }
+}
+
+const root = new Node('*', new Node('+', new Node(3), new Node(2)), new Node('+', new Node(4), new Node(5)));
+const res = evaluation(root);
+
+console.log({ res })
 
 // Recursively go through tree
 //  if null return 0
