9a5ba10562ded5ea875d143d8d43d52a8e0670f0
Alejandro Chang <alejandrochang10@gmail.com>
2023-06-13 23:05:04 -0700

    alg: por expression simplification

--- DailyProblems/por.js
+++ DailyProblems/por.js
@@ -1,2 +1,27 @@
 // Schema + Type JS Problem
 
+// input: '5', { type: string } - true
+// input: 5, { type: number } - true
+// input: [], { type: array, input: { type: 'string' } } - true
+
+
+const validateSchema = {
+  string: (s) => typeof s === 'string',
+  number: (n, min, max) => {
+    const expression = min && max ? (min < n && n < max) : (min && !max ? n < min : n > min);
+    return typeof n === 'number' && expression;
+  },
+  array: (n) => Array.isArray(n),
+  object: (n) => typeof n === 'object',
+}
+
+console.log(
+  'String Validation:', validateSchema.string('5'), // true
+  'Number Validation:', validateSchema.number(5), // true
+  'Array Validation:', validateSchema.array([1,2,3]), // true
+  'Object Validation:', validateSchema.object({ num: 5}), // true
+)
+
+console.log(
+  'Number Validation:', validateSchema.number(5, 1, 10), // true
+)
