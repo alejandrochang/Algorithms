d2a9790392d3cb34d088440979c775d1815eaa8a
Alejandro Chang <alejandrochang10@gmail.com>
2023-06-15 08:52:06 -0700

    alg: mo promises testing

--- DailyProblems/promrefresh.js
+++ DailyProblems/promrefresh.js
@@ -3,3 +3,20 @@
 // setTimeout Delay
 // new promise((res, rej)) syntax
 
+async function test(num) {
+  if (num > 1) return num + 1;
+  throw num - 1;
+}
+
+// console.log(test(4)); // Promise { 5 }
+// console.log(test(-1)); // rejected with reason -2
+
+function printTest(fn) {
+  fn.then((v) => {
+    console.log({ v })
+  }).catch(e => console.log({ e }));
+}
+
+console.log(printTest(test(4))); // v: 5
+console.log(printTest(test(1))); // e: 0
+
