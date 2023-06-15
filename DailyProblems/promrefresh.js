34262da61cd8e6fed3dc3dc5dba9d0cab51cbbe4
Alejandro Chang <alejandrochang10@gmail.com>
2023-06-15 09:31:05 -0700

    alg: more prom testing

--- DailyProblems/promrefresh.js
+++ DailyProblems/promrefresh.js
@@ -11,18 +11,44 @@ async function test(num) {
 // console.log(test(4)); // Promise { 5 }
 // console.log(test(-1)); // rejected with reason -2
 
+// then/catch
 test(4).then((v) => {
-    console.log({ v })
+    // console.log({ v })
   }).catch(e => console.log({ e })); // v: 5
 
-test(1).then((v) => {
-    console.log({ v })
-  }).catch(e => console.log({ e })); // v: 0
+// test(1).then((v) => {
+//     // console.log({ v })
+//   }).catch(e => console.log({ e })); // v: 0
 
+
+// async/await
 async function asyncCall() {
   const result = await test(5); // 6
   // const result2 = await test(1); // error
-  console.log({ result });
+  // console.log({ result });
 }
 
-asyncCall();
+// asyncCall();
+
+
+// new Promise
+const handleNewPromise = (num) => {
+  return new Promise((resolve, reject) => {
+    if (num > 1) resolve(num + 1);
+    reject(num - 1)
+  })
+}
+// console.log('plop', handleNewPromise(5));
+
+const delay = t => new Promise((res, rej) => setTimeout(res, t));
+delay(5000).then(() => console.log('hello'));
+
+// const promiseA = new Promise((resolve, reject) => {
+//   try {
+//     resolve(777);
+//   } catch (e) {
+//     reject(e);
+//   }
+// });
+
+// promiseA.then((val) => console.log("asynchronous logging has val:", val));
