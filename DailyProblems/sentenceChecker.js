6c6e9048b1b12123b84266bf2477d74227b92bf6
Alejandro Chang <alejandrochang10@gmail.com>
2023-06-20 23:53:55 -0700

    alg: gen solution sentence problem

--- DailyProblems/sentenceChecker.js
+++ DailyProblems/sentenceChecker.js
@@ -7,3 +7,26 @@
 // All other characters must be lowercase letters, separators (,,;,:) or terminal marks (.,?,!,‽).
 // There must be a single space between each word.
 // The sentence must end with a terminal mark immediately following a word.
+
+const checkUperCase = (s) => s.toUpperCase();
+const checkPeriod = (s) => s === '.';
+
+// const checkValidWord = (word) => word;
+
+function sentenceChecker(str) {
+  if (str[0] !== checkUperCase(str[0])) return false;
+  if (str[str.length - 1] !== checkPeriod(str[str.length - 1])) return false;
+
+  // const words = str.split(' ');
+}
+
+const sen = 'Check if this is valid sentence'; // true
+const res = sentenceChecker(sen);
+
+console.log({ res });
+
+
+// 1. Check if str starts with capital word
+// 2. Ensure all other chars are lower case or special chars (,,;,:) or terminal marks (.,?,!,‽)
+// 3. Ensure space between words (check each word)
+// 4. Ensure last ch ends with period
