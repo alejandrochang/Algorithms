2ac53a3daa40114a116c1ccfc9d12f904f217b74
Alejandro Chang <alejandrochang10@gmail.com>
2023-06-26 22:04:55 -0700

    alg: further prac

--- DailyProblems/histogram.js
+++ DailyProblems/histogram.js
@@ -9,6 +9,6 @@
 // x x x x
 
 // Determine the area of the largest rectangle that can be formed only from the bars of the histogram.
-//  For the diagram above, for example,
+// For the diagram above, for example,
 // this would be six, representing the 2 x 3 area at the bottom right.
 
