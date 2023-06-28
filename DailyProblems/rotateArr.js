7db25b48d539545f52dcf1acf8ae1fd24f6ceee5
Alejandro Chang <alejandrochang10@gmail.com>
2023-06-27 20:51:45 -0700

    alg: problems

---
+++ DailyProblems/rotateArr.js
@@ -0,0 +1,18 @@
+// This problem was asked by Amazon.
+
+// Given an array and a number k that's
+// smaller than the length of the array, rotate the array to the right k elements in-place.
+
+const arr = [1,2,3,4]; // [4,1,2,3];
+const k = 3;
+
+// arr[length - 1] = 3
+// arr.length - k - 1 = 0
+
+function rotateInPlace(arr, k) {
+  
+}
+
+const result = rotateInPlace(arr, k);
+console.log({ result });
+
