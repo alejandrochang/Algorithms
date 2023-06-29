0b5c2aa0d8bf99101f03bf5c12ba0295029521ea
Alejandro Chang <alejandrochang10@gmail.com>
2023-06-28 23:59:40 -0700

    alg: solution

--- DailyProblems/nextBiggerBST.js
+++ DailyProblems/nextBiggerBST.js
@@ -11,19 +11,31 @@
 
 // You can assume each node has a parent pointer.
 
-function nextBiggerNode(root) {
+function nextBiggerNode(node) {
   if (!root) return;
+  const inorderTraverse = (n) => {
+    if (!n) return;
+    inorderTraverse(n.left);
+    if (node.data === n.data) {
+      return n.data.parent;
+    }
+    inorderTraverse(n.right);
+  }
+
+  return inorderTraverse(node);
 }
 
 
 class Node {
-  constructor(data, left=null, right=null) {
+  constructor(data, left=null, right=null, parent=null) {
     this.data = data;
     this.left = left;
     this.right = right;
+    this.parent = parent;
   }
 }
 
 const root = new Node(10, new Node(5), new Node(30, new Node(22), new Node(35)));
-const res = nextBiggerNode(root);
+const chosenNode = new Node(30);
+const res = nextBiggerNode(chosenNode);
 console.log({ res }); // 22
