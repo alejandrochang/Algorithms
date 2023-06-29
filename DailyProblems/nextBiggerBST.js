9293fab503a012930a0be15dc2979d9801c6ba73
Alejandro Chang <alejandrochang10@gmail.com>
2023-06-28 23:56:02 -0700

    alg: setting up problem

--- DailyProblems/nextBiggerBST.js
+++ DailyProblems/nextBiggerBST.js
@@ -1,4 +1,5 @@
-// Given a node in a binary search tree, return the next bigger element, also known as the inorder successor.
+// Given a node in a binary search tree, return the next bigger element,
+//  also known as the inorder successor.
 
 // For example, the inorder successor of 22 is 30.
 
@@ -7,4 +8,22 @@
 //  5    30
 //      /  \
 //    22    35
+
 // You can assume each node has a parent pointer.
+
+function nextBiggerNode(root) {
+  if (!root) return;
+}
+
+
+class Node {
+  constructor(data, left=null, right=null) {
+    this.data = data;
+    this.left = left;
+    this.right = right;
+  }
+}
+
+const root = new Node(10, new Node(5), new Node(30, new Node(22), new Node(35)));
+const res = nextBiggerNode(root);
+console.log({ res }); // 22
