e8e3227ccf550e8442c89e362c9488970d8fdc29
Alejandro Chang <alejandrochang10@gmail.com>
2023-07-01 18:17:20 -0700

    alg: more setup of problem

--- DailyProblems/convertBTree.js
+++ DailyProblems/convertBTree.js
@@ -18,3 +18,31 @@
 // 5         4
 //         /   \
 //        6     7
+
+// I: root node
+// O: binary tree
+
+// High level:
+// OK: If left and right (ok) + if (left and right === null)
+// Remove: if left | right (remove) - if
+
+function convertFullBinaryTree(root){
+  if (!root) return;
+
+}
+
+class Node {
+  constructor(data, left=null, right=null) {
+    this.data = data;
+    this.left = left;
+    this.right = right;
+  }
+}
+
+const root = new Node(0,
+  new Node(1, new Node(3, new Node(5))),
+  new Node(2, new Node(4, new Node(4), new Node(7)))
+);
+
+const result = convertFullBinaryTree(root);
+console.log({ result });
