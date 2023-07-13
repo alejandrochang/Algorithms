3dedce7f9114a3eddd040e52781b9f6da4f9a53e
Alejandro Chang <alejandrochang10@gmail.com>
2023-07-13 16:59:06 -0700

    alg: thinking of problem

--- DailyProblems/maxWeightPath.js
+++ DailyProblems/maxWeightPath.js
@@ -10,10 +10,21 @@
 //  For example, 1 -> 3 -> 5. The weight of the path is the sum of the entries.
 
 
-function maxWeightPath(arr){
+// ALl sums: 4, 8, 9, 5 
 
+class Node {
+  constructor(val, left=null,right=null) {
+    this.val = val;
+    this.left = left;
+    this.right = right;
+  }
+}
+
+function maxWeightPath(arr){
+  
 }
 
 
 const test = maxWeightPath([[1], [2, 3], [1, 5, 1]]); // 
 console.log(maxWeightPath(test));
+
