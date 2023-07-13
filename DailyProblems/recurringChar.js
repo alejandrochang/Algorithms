a112fcba50d7bd41a4349103f606f8c092fff0f6
Alejandro Chang <alejandrochang10@gmail.com>
2023-07-13 16:43:59 -0700

    alg: test examples

--- DailyProblems/recurringChar.js
+++ DailyProblems/recurringChar.js
@@ -2,3 +2,14 @@
 
 // For example, given the string "acbbac", return "b".
 // Given the string "abcdef", return null.
+
+function recurringChar(str) {
+  
+}
+
+
+const test = "acbbac";
+const test2 = "abcdef";
+
+console.log(recurringChar(test)); // 'b'
+console.log(recurringChar(test2)); // null
