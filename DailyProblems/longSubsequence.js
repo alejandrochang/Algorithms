6349fe7faaf0b64c242c4dba522bfaea8cd34416
Alejandro Chang <alejandrochang10@gmail.com>
2023-07-15 17:43:31 -0700

    alg: solution lsl

--- DailyProblems/longSubsequence.js
+++ DailyProblems/longSubsequence.js
@@ -6,17 +6,41 @@
 // the longest increasing subsequence has length 6: it is 0, 2, 6, 9, 11, 15.
 
 function longestIncreasingSubsequence(arr) {
-  let max = 0;
+  return arr.reduce((seq, num) => {
+    if (num > seq[seq.length - 1]) {
+      seq.push(num);
+    } else {
+      // if not greater than next num - we swap for smallest
+      const idx = seq.findIndex(val => val >= num);
+      seq[idx] = num;
+    }
 
-  for (let i = 0; i < arr.length; i++) {
-    let currentMax = Math.max(max, arr[i]);
-    max = Math.max(currentMax + max, max);
-  }
+    console.log({ seq });
+    return seq;
+  }, [arr[0]]).length;
 }
 
 const arr = [0, 8, 4, 12, 2, 10, 6, 14, 1, 9, 5, 13, 3, 11, 7, 15];
 const result = longestIncreasingSubsequence(arr);
-// 6
 
 // return length; not the sum
-console.log({ result });
+// console.log({ result }); // 6 - 0, 2, 6, 9, 11, 15.
+
+
+function longestIncreasingSubsequenceSum(arr) {
+  return arr.reduce((seq, num) => {
+    if (num > seq[seq.length - 1]) {
+      seq.push(num);
+    } else {
+      // we need to substitute last smallest
+      const idx = seq.findIndex(val => val >= num);
+      seq[idx] = num;
+    }
+
+    return seq;
+  }, [arr[0]]);
+}
+
+const res2 = longestIncreasingSubsequenceSum(arr);
+// console.log({ res2 }); // 43
+
