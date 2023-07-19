2987fb9dcd2162601823345627fbef5e7b79390c
Alejandro Chang <alejandrochang10@gmail.com>
2023-07-19 15:42:28 -0700

    alg: ee solution

--- DailyProblems/eventEmitter.js
+++ DailyProblems/eventEmitter.js
@@ -11,3 +11,51 @@
 
 // The above snippet is in Typescript, but write your answer in 
 // Javascript and just assume your class methods will always get called with the correct JS types.
+
+class EventEmitter {
+  constructor() {
+    this.events = {};
+  }
+
+  on(eventName, cb, times) {
+    // handle case where no event name is present
+    if (this.events[eventName]) {
+      this.events[eventName] = [...this.events[eventName], { cb, times }]
+    } else {
+      this.events[eventName] = [{ cb, times }];
+    }
+  }
+
+  emit(eventName, ...args) {
+    const handler = this.events[eventName];
+    if (!handler) return;
+
+    handler.forEach((e) => {
+      e.cb(...args)
+      e.times -= 1;
+    })
+
+    this.events[eventName] = handler.filter((e) => e.times);
+  }
+
+  removeListener(eventName, cb) {
+    const handler = this.events[eventName];
+    if (!handler) return;
+
+    const filtered = handler.filter(({ cb: _fn }) => JSON.stringify(_fn) !== JSON.stringify(cb));
+    this.events[eventName] = filtered;
+  }
+}
+
+const e = new EventEmitter();
+
+e.on('event1', () => console.log('hello world'), 2);
+e.on('event1', () => console.log('hello other world'), 2);
+e.on('event2', () => console.log('plop'), 10);
+
+e.emit('event1'); // hello world/hello other world
+e.removeListener('event1', () => console.log('hello world'));
+e.emit('event1'); // hello world/hello other world (null if e.remove)
+// e.emit('event2'); // plop
+e.emit('event1'); // null
+e.emit('event1'); // null
