771a62bdf5a4e063d32a94e6fd4c56839949fed0
Alejandro Chang <alejandrochang10@gmail.com>
2023-07-21 11:09:36 -0700

    alg(ctci): ll setup

---
+++ CTCI/chapter02/2.0-LinkedList.js
@@ -0,0 +1,20 @@
+
+// Node { next, data };
+
+class Node {
+  constructor(data, next=null) {
+    this.data = data;
+    this.next = next;
+  }
+}
+
+// linked list methods - size, clear, getLast, getFirst
+class LinkedList {
+  constructor(head=null) {
+    this.head = head;
+  }
+
+  size() {
+    let count = 0;
+  }
+}
