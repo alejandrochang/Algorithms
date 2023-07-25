a358456c6b639d0a3e8b0b9d71cce74ed0b17155
Alejandro Chang <alejandrochang10@gmail.com>
2023-07-24 18:49:54 -0700

    alg(ctci): delete middle node solution

--- CTCI/chapter02/2.3-deleteMiddleNode.js
+++ CTCI/chapter02/2.3-deleteMiddleNode.js
@@ -9,20 +9,25 @@ for (let num of arr) ll.insertFirst(num);
 
 
 function deleteMiddleNode(list) {
+  let prev = null;
   let slow = list.head;
-  let fast = slow.next.next;
+  let fast = list.head.next;
 
-  while (fast.next) {
+  while (fast.next && fast.next.next) {
+    prev = slow;
     slow = slow.next;
     fast = fast.next;
   }
 
-  slow.next = fast;
+  prev.next = slow.next;
+  slow = slow.next;
   return list;
 }
 
 // 1 -> 2 -> 3   if (!fast.next) slow.next = fast
 // 1 -> 2 -> 3 -> 4  
+
+// let prev, slow and fast
 
 const result = deleteMiddleNode(ll);
 console.log('res', JSON.stringify(result, null, 4));
