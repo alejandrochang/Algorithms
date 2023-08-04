6c26de1c06c5e4512ec5bb06f2195605feda97de
Alejandro Chang <alejandrochang10@gmail.com>
2023-08-03 17:07:05 -0700

    alg: adding new algorithms

---
+++ nvPractice/degreeArr.js
@@ -0,0 +1,8 @@
+// Input: nums = [1,2,2,3,1]
+// Output: 2
+// Explanation: 
+// The input array has a degree of 2 because both elements 1 and 2 appear twice.
+// Of the subarrays that have the same degree:
+// [1, 2, 2, 3, 1], [1, 2, 2, 3], [2, 2, 3, 1], [1, 2, 2], [2, 2, 3], [2, 2]
+// The shortest length is 2. So return 2.
+
