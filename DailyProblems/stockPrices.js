0ec81581c986f9d9144a04db19a6743551621985
Alejandro Chang <alejandrochang10@gmail.com>
2023-08-07 10:52:32 -0700

    setup of problem

---
+++ DailyProblems/stockPrices.js
@@ -0,0 +1,44 @@
+// Given a array of numbers representing the stock prices of a company in chronological order,
+//  write a function that calculates the maximum profit you 
+// could have made from buying and selling that stock once. You must buy before you can sell it.
+
+// For example, given [9, 11, 8, 5, 7, 10], you should return 5, 
+// since you could buy the stock at 5 dollars and sell it at 10 dollars.
+
+const stocks = [9, 11, 8, 5, 7, 10];
+
+// start: 9, end: 11, value: 2
+// if < start -> start is 8 -> 5
+// start: 5, end: 7, value: 2
+// start: 5, end: 10, value = 5
+
+// function kadanes(prices) {
+//   let ans = 0;
+//   let sum = 0;
+
+//   for (let i=1; i < prices.length; i++) {
+//     ans = Math.max(0, ans + prices[i]);
+//     sum = Math.max(sum, ans);
+//   }
+
+//   return sum;
+// }
+
+function stockPrices(prices) {
+  
+}
+
+const res = stockPrices(stocks);
+console.log({ res });
+
+
+
+// const stocks = [9, 11, 8, 5, 7, 10];
+
+/* 
+val = 3
+  i = 10 (move i if > i)
+  j = i - 1; 7
+  diff = 10 - 7; 3  (j--)
+
+*/
