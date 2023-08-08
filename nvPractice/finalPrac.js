b93511b5f5138e9e404b5dad5ba3f33aa476b653
Alejandro Chang <alejandrochang10@gmail.com>
2023-08-08 14:49:07 -0700

    alg: speed run prac : :

---
+++ nvPractice/finalPrac.js
@@ -0,0 +1,77 @@
+// 1. PivotX
+// The first part contains all elements in lst that are less than x
+// The second part contains all elements in lst that are equal to x
+// The third part contains all elements in lst that are larger than x
+// Ordering within a part can be arbitrary.
+
+// For example, given x = 10 and lst = [9, 12, 3, 5, 14, 10, 10], one partition may be [9, 3, 5, 10, 10, 12, 14].
+
+
+
+// 2. Deepest Node
+
+//   a
+//    / \
+//   b   c
+//  /
+// d
+
+// Return deepest node: 'd'
+
+
+
+
+// 3. Valid Soduku
+// Determine if a 9 x 9 Sudoku board is valid. 
+// Only the filled cells need to be validated according to the following rules:
+
+// Each row must contain the digits 1-9 without repetition.
+// Each column must contain the digits 1-9 without repetition.
+// Each of the nine 3 x 3 sub-boxes of the grid must contain the digits 1-9 without repetition.
+// Note:
+
+// A Sudoku board (partially filled) could be valid but is not necessarily solvable.
+// Only the filled cells need to be validated according to the mentioned rules.
+// const board = 
+// [["5","3",".",".","7",".",".",".","."]
+// ,["6",".",".","1","9","5",".",".","."]
+// ,[".","9","8",".",".",".",".","6","."]
+// ,["8",".",".",".","6",".",".",".","3"]
+// ,["4",".",".","8",".","3",".",".","1"]
+// ,["7",".",".",".","2",".",".",".","6"]
+// ,[".","6",".",".",".",".","2","8","."]
+// ,[".",".",".","4","1","9",".",".","5"]
+// ,[".",".",".",".","8",".",".","7","9"]];
+
+// // Output: true
+
+// const board2 = 
+// [["8","3",".",".","7",".",".",".","."]
+// ,["6",".",".","1","9","5",".",".","."]
+// ,[".","9","8",".",".",".",".","6","."]
+// ,["8",".",".",".","6",".",".",".","3"]
+// ,["4",".",".","8",".","3",".",".","1"]
+// ,["7",".",".",".","2",".",".",".","6"]
+// ,[".","6",".",".",".",".","2","8","."]
+// ,[".",".",".","4","1","9",".",".","5"]
+// ,[".",".",".",".","8",".",".","7","9"]];
+// Output: false
+
+
+
+
+
+
+
+
+
+
+
+// 4. Degree Arr
+// Input: nums = [1,2,2,3,1]
+// Output: 2
+// Explanation: 
+// The input array has a degree of 2 because both elements 1 and 2 appear twice.
+// Of the subarrays that have the same degree:
+// [1, 2, 2, 3, 1], [1, 2, 2, 3], [2, 2, 3, 1], [1, 2, 2], [2, 2, 3], [2, 2]
+// The shortest length is 2. So return 2.
