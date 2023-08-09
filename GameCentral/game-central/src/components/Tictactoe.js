298d9d95947919c775efc49ac95e211b724b7fe8
Alejandro Chang <alejandrochang10@gmail.com>
2023-08-08 23:35:02 -0700

    tic tac toe problem

--- GameCentral/game-central/src/components/Tictactoe.js
+++ GameCentral/game-central/src/components/Tictactoe.js
@@ -18,32 +18,67 @@ if (temp) board.push(temp);
 
 const TicTacToe = () => {
   const [boardState, setBoard] = useState(board);
-  const [currPlayer, setCurrPlayer] = useState('player1');
+  const [currPlayer, setCurrPlayer] = useState({ name: 'player1', move: 'X'});
+  const [hasWon, setHasWon] = useState(false);
+
+  const handleWon = () => {
+    // handle win logic (checkRow, checkCol, checkDiagnol)
+    const checkWon = (list) => {
+      if (list.every(el => el === 'X')) setHasWon(true);
+      if (list.every(el => el === 'O')) setHasWon(true);
+    }
+
+    let columns = [[],[],[]];
+    let z = 0;
+    for (let i = 0; i < boardState.length; i++) {
+      // check row
+      checkWon(boardState[i]);
+      for (let j = 0; j < boardState[0].length; j++) {
+        let col = boardState[j][i];
+        if (columns[z].length < 3) {
+          columns[z].push(col);
+        } else {
+          z++;
+          columns[z].push(col);
+        }
+      }
+    }
+
+    columns.map((row) => checkWon(row));
+  }
 
   const handlePlayerChange = () => {
-    if (currPlayer === 'player1') {
-      setCurrPlayer('player2');
+    if (currPlayer.name === 'player1') {
+      setCurrPlayer({ name: 'player2', move: 'O'});
     } else {
-      setCurrPlayer('player1');
+      setCurrPlayer({ name: 'player1', move: 'X'});
     }
   }
 
-  const handlePositionChange = () => {};
-  // console.log({ board });
-  const Square = ({ r }) => <div className="toe-row">{r.map((c) => <div className="toe-cell">{c}</div>)}</div>
+
+  const handlePositionMark = (r, c) => {
+    boardState[r][c] = currPlayer.move;
+    setBoard(boardState);
+    const won = handleWon();
+    console.log('plop', won);
+    handlePlayerChange(currPlayer);
+  };
+
+  const Square = ({ r, rI }) => <div className="toe-row">{r.map((c, cI) => <div onClick={() => handlePositionMark(rI,cI)} className="toe-cell">{c}</div>)}</div>
   return (
     <>
       <div className='toe-container'>
         <div className='toe-title'>
           <h4>Tic Tac Toe</h4>
         </div>
+        {hasWon && <div>Congrats you won!</div>}
         <div className='board-container'>
-          {boardState.map((row) => {
-            return <Square r={row} />
+          {boardState.map((row, rI) => {
+            return <Square r={row} rI={rI} />
           })}
         </div>
         <div className='footer'>
-          <span>Current {currPlayer} turn</span>
+          <span>Current {currPlayer.name} turn</span>
         </div>
       </div>
     </>
