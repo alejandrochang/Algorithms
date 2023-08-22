ecb7814df5ece3920c6a68246d7506a674cef680
Alejandro Chang <alejandrochang10@gmail.com>
2023-08-21 19:17:15 -0700

    mo prac

--- DailyProblems/ptehorem.js
+++ DailyProblems/ptehorem.js
@@ -2,3 +2,17 @@
 // Recall that a Pythogorean triplet(a, b, c) is defined by the equation a2 + b2= c2.
 
 
+function ptheorem(arr) {
+  // Find the shortest gap
+  const sorted = arr.sort();
+
+  // for (let i = 0; i < sorted.length; i++) {
+  //   let a = sorted[i];
+  //   let b = sorted[i + 1];
+  //   let c = sorted[i + 2];
+
+  // }
+}
+
+const arr = [1,2,3];
+console.log('res', ptheorem(arr));
