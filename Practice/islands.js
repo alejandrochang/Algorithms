7e0ee0885568efd6a3bff05f4dc3f105e16c3757
Alejandro Chang <alejandrochang10@gmail.com>
2023-09-15 11:02:12 +0200

    alg: island problem sol

--- Practice/islands.js
+++ Practice/islands.js
@@ -1,12 +1,39 @@
 /*
   High Level Approach: 
   1. DFS approach from starting position, traverse every node
-
+  2. see if left, right, up, down is equal to 1
 */
 
 
-function islands(board) {
+function islands(grid) {
+  if (!grid.length) return 0;
 
+  function dfs(i, j) {
+    // Base case for out of bounds or if equal to 0
+    if (i < 0 || j < 0 || i >= grid.length || j >= grid[0].length || grid[i][j] === '0') return;
+
+    grid[i][j] = '0';
+
+    // traverse four directions
+    dfs(i-1,j);
+    dfs(i+1,j);
+    
+    dfs(i,j-1);
+    dfs(i,j+1);
+  }
+
+  let count = 0;
+  for (let i = 0; i < grid.length; i++) {
+    for (let j = 0; j < grid[0].length; j++) {
+      // only traverse when we have the island
+      if (grid[i][j] === '1') {
+        count += 1;
+        dfs(i, j)
+      }
+    }
+  }
+
+  return count;
 }
 
 const grid1 = [
